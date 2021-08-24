e03f712ed8e5db8200be1be78ee1fce252f03cbd
cwild <conor.wild@sky.com>
2021-08-24 14:32:28 +0100

    m

---
+++ scripts/rsync_to_condor.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import fire
+from pymongo import MongoClient
+
+from pandda_lib import constants
+
+
+def main():
+    client = MongoClient()
+
+    mongo_diamond_paths = client[constants.mongo_diamond_paths]
+
+    for system in mongo_diamond_paths.find():
+        print(system)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
