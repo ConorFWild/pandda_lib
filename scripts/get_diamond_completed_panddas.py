2748ce20c51ab8745654b00dd1d7cde9e0b69b15
cwild <conor.wild@sky.com>
2021-08-24 09:57:27 +0100

    m

--- scripts/get_diamond_completed_panddas.py
+++ scripts/get_diamond_completed_panddas.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 import fire
+import pymongo
+
 
 from pandda_lib.fs import XChemDiamondFS
 
@@ -15,8 +17,23 @@ def main(diamond_dir: str, output_dir: str):
 
     print(diamond_fs.model_building_dirs)
 
-    print(",".join([str(x) for x in diamond_fs.model_building_dirs.values()]))
-    print(",".join([str(x) for x in diamond_fs.pandda_dirs.values()]))
+    # print(",".join([str(x) for x in diamond_fs.model_building_dirs.values()]))
+    # print(",".join([str(",") for x in diamond_fs.pandda_dirs.values()]))
+
+    client = pymongo.MongoClient()
+
+    diamond_paths = client.pandda.diamond_paths
+
+    for system_name, model_building_dir in diamond_fs.model_building_dirs.items():
+        doc = {
+                "system_name": system_name,
+                "model_bulding_dir": model_building_dir,
+                "pandda_dirs": diamond_fs.pandda_dirs[system_name],
+            }
+        print(
+            doc
+        )
+
 
 
 if __name__ == "__main__":
