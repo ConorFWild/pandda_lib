acc67808264383c439457bbfdf7dc8840d50b707
cwild <conor.wild@sky.com>
2021-08-27 10:58:18 +0100

    m

---
+++ pandda_lib/mongo/pandda.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from mongoengine import (
+    Document,
+    StringField,
+    ListField,
+    EmbeddedDocumentField,
+    ReferenceField,
+    IntField,
+    FloatField
+)
+
+
+class Structure:
+    path: StringField()
+
+
+class Reflections:
+    path: StringField()
+
+
+class Compound:
+    path: StringField()
+
+
+class System:
+    system_name: StringField()
+    datasets: ListField(ReferenceField(Dataset))
+
+
+class Dataset(Document):
+    dtag: StringField()
+    system: ReferenceField(System)
+    structure: ReferenceField(Structure)
+    reflections: ReferenceField(Reflections)
+    compounds: ListField(ReferenceField(Compound))
+
+
+class Model:
+    path: StringField()
+    dataset: ReferenceField(Dataset)
+    compounds: ListField(ReferenceField(Compound))
+
+
+class Event(Document):
+    dataset: ReferenceField(Dataset)
+    event_idx: IntField()
+    x: FloatField()
+    y: FloatField()
+    z: FloatField()
+    model: ReferenceField(Model)
+
+
+class PanDDA:
+    path: StringField()
+    system: ReferenceField(System)
+    events: ListField(ReferenceField(Event))
+    datasets: ListField(StringField)
---
+++ scripts/mongo_populate.py
@@ -0,0 +1,111 @@
+from pathlib import Path
+
+import pandas as pd
+import fire
+import mongoengine
+
+from pandda_lib.mongo.pandda import *
+from pandda_lib import constants
+
+def add_pandda():
+    ...
+
+def main(data_dirs, pandda_dirs):
+
+    data_dirs = Path(data_dirs)
+    pandda_dirs = Path(pandda_dirs)
+
+    for data_dir in data_dirs.glob("*"):
+        system_name = data_dir.name
+        system = System(
+            system_name=system_name,
+        )
+
+        system_datasets = []
+        for dataset_dir in data_dir.glob("*"):
+
+            dtag = dataset_dir.name
+            reflections_path = dataset_dir / "dimple.mtz"
+            structure_path = dataset_dir / "dimple.pdb"
+            compound_dir = dataset_dir / "compound"
+            compound_paths = compound_dir.glob("*.cif")
+
+            reflections = Reflections(path=str(reflections_path))
+            structure = Structure(path=str(structure_path))
+            compounds = [Compound(path=str(compound_path)) for compound_path in compound_paths]
+
+            dataset = Dataset(
+                dtag=dtag,
+                system=system,
+                structure=structure,
+                reflections=reflections,
+                compounds=compounds,
+            )
+            system_datasets.append(dataset)
+
+        system.datasets = system_datasets
+        system.save(cascade=True)
+
+    for pandda_dir in pandda_dirs.glob("*"):
+        # system_name = pandda_dir.name
+        # system = System.objects(system_name=system_name)[0]
+
+        processed_datasets_dir = pandda_dir / constants.PANDDA_PROCESSED_DATASETS_DIR
+        pandda_datasets = []
+        pandda_models = []
+        for dataset_dir in processed_datasets_dir.glob("*"):
+            dtag = dataset_dir.name
+            dataset = Dataset.objects(dtag=dtag)[0]
+
+            model_dirs_path = dataset_dir / constants.PANDDA_MODELLED_STRUCTURES_DIR
+            model_path = model_dirs_path / constants.PANDDA_EVENT_MODEL.format(dtag)
+
+            model = Model(
+                path=str(model_path),
+                dataset=dataset,
+            )
+            model.save(cascade=True)
+            pandda_models.append(model)
+            pandda_datasets.append(dataset)
+
+        analysis_dir_path = pandda_dir / constants.PANDDA_ANALYSES_DIR
+        event_table_path = analysis_dir_path / constants.PANDDA_ANALYSE_EVENTS_FILE
+
+        event_table = pd.read_csv(event_table_path)
+        pandda_events = []
+        for index, event_record in event_table.iterrows():
+            print(event_record)
+            dtag = event_record["dtag"]
+            dataset = Dataset.objects(dtag=dtag)[0]
+            event_idx = event_record["event_idx"]
+            x = event_record["x"]
+            y= event_record["y"]
+            z = event_record["z"]
+            model = Model.objects(
+                dataset=dataset,
+            )
+            event = Event(
+                datasset=pandda_datasets,
+                event_idx=event_idx,
+                x=x,
+                y=y,
+                z=z,
+                model=model,
+            )
+            pandda_events.append(event)
+
+        pandda_system = pandda_datasets[0].system
+
+        pandda = PanDDA(
+            path=str(pandda_dir),
+            system=pandda_system,
+            events=pandda_events,
+            datasets=pandda_datasets,
+        )
+
+        pandda.save(cascade=True)
+
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
