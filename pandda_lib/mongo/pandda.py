7f9c4490ff37295af52457cb590d8a74ff8c842f
cwild <conor.wild@sky.com>
2021-08-27 12:21:44 +0100

    m

--- pandda_lib/mongo/pandda.py
+++ pandda_lib/mongo/pandda.py
@@ -10,47 +10,47 @@ from mongoengine import (
 
 
 class Structure:
-    path: StringField()
+    path = StringField()
 
 
 class Reflections:
-    path: StringField()
+    path = StringField()
 
 
 class Compound:
-    path: StringField()
+    path = StringField()
 
 
 class System:
-    system_name: StringField()
-    datasets: ListField(ReferenceField("Dataset"))
+    system_name = StringField()
+    datasets = ListField(ReferenceField("Dataset"))
 
 
 class Dataset(Document):
-    dtag: StringField()
-    system: ReferenceField(System)
-    structure: ReferenceField(Structure)
-    reflections: ReferenceField(Reflections)
-    compounds: ListField(ReferenceField(Compound))
+    dtag = StringField()
+    system = ReferenceField(System)
+    structure = ReferenceField(Structure)
+    reflections = ReferenceField(Reflections)
+    compounds = ListField(ReferenceField(Compound))
 
 
 class Model:
-    path: StringField()
-    dataset: ReferenceField(Dataset)
-    compounds: ListField(ReferenceField(Compound))
+    path = StringField()
+    dataset = ReferenceField(Dataset)
+    compounds = ListField(ReferenceField(Compound))
 
 
 class Event(Document):
-    dataset: ReferenceField(Dataset)
-    event_idx: IntField()
-    x: FloatField()
-    y: FloatField()
-    z: FloatField()
-    model: ReferenceField(Model)
+    dataset = ReferenceField(Dataset)
+    event_idx = IntField()
+    x = FloatField()
+    y = FloatField()
+    z = FloatField()
+    model = ReferenceField(Model)
 
 
 class PanDDA:
-    path: StringField()
-    system: ReferenceField(System)
-    events: ListField(ReferenceField(Event))
-    datasets: ListField(StringField)
+    path = StringField()
+    system = ReferenceField(System)
+    events = ListField(ReferenceField(Event))
+    datasets = ListField(StringField)
