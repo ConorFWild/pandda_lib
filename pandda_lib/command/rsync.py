6fdd7a73c83986f4ef288bae4430b504a4984bd7
cwild <conor.wild@sky.com>
2021-09-01 12:31:25 +0100

    m

--- pandda_lib/command/rsync.py
+++ pandda_lib/command/rsync.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from pathlib import Path
 import subprocess
 
-command: str = "sshpass -p '{password}' rsync --progress --exclude 'autoprocessing' --exclude 'dimple' --exclude 'jpg' --exclude '*.map' --exclude 'processed' --exclude '*.png' --exclude '*.jpg' -L -avzh -e ssh zoh22914@ssh.diamond.ac.uk:{path_to_remote_dir}/ {path_to_loca_dir}"
+command: str = "sshpass -p '{password}' rsync --progress --exclude 'autoprocessing' --exclude 'dimple' --exclude 'jpg' --exclude '*.map' --exclude 'processed' --exclude '*.png' --exclude '*.pickle' --exclude '*.jpg' -L -avzh -e ssh zoh22914@ssh.diamond.ac.uk:{path_to_remote_dir}/ {path_to_loca_dir}"
 
 
 @dataclass()
