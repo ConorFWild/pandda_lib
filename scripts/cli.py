cf8b062d36506b59bb518891e01e4078bf994090
cwild <conor.wild@sky.com>
2021-09-01 16:39:09 +0100

    m

--- scripts/cli.py
+++ scripts/cli.py
@@ -1,11 +1,29 @@
+import json
+
 from prompt_toolkit.shortcuts import button_dialog
 
-result = button_dialog(
-    title='Button dialog example',
-    text='Do you want to confirm?',
-    buttons=[
-        ('Yes', True),
-        ('No', False),
-        ('Maybe...', None)
-    ],
-).run()
+import mongo_populate
+
+if __name__ == "__main__":
+
+    config_file = "cli.json"
+
+    with open(config_file, "r") as f:
+        config = json.load(f)
+
+    functions = {
+        "mongo_populate": lambda: mongo_populate.main(
+            config["mongo_populate"]["model_dirs"],
+            config["mongo_populate"]["pandda_dirs"],
+        )
+    }
+
+    f = button_dialog(
+        title='Button dialog example',
+        text='Do you want to confirm?',
+        buttons=[
+            [(name, name) for name in config]
+        ],
+    ).run()
+
+    f()
