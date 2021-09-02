a3fd3720db74359ea47b21005874cb368879ee65
cwild <conor.wild@sky.com>
2021-09-02 11:31:31 +0100

    m

--- pandda_lib/command/pandda_2_command.py
+++ pandda_lib/command/pandda_2_command.py
@@ -3,8 +3,6 @@ from dataclasses import dataclass
 from typing import *
 from pathlib import Path
 
-from pandda_lib.command import ShellCommand
-
 
 class PanDDA2Command:
     def __init__(self,
@@ -23,5 +21,3 @@ class PanDDA2Command:
                  ):
         self.command = f"python {analyse_path} {data_dirs} {out_dir} --pdb_regex='{pdb_regex}' --mtz_regex='{mtz_regex}' --structure_factors='(\"{structure_factors_f}\",\"{structure_factors_phi}\")' --autobuild={autobuild} --global_processing='{global_processing}' --local_cpus={local_cpus} --distributed_mem_per_core={mem_per_core} --scheduler={distributed_scheduler}"
 
-    def run(self):
-        ShellCommand(self.command).run()
