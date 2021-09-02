ad2788cb3e6762189f36e34d30404063ace3ed39
cwild <conor.wild@sky.com>
2021-09-02 11:46:48 +0100

    m

--- pandda_lib/command/pandda_2_command.py
+++ pandda_lib/command/pandda_2_command.py
@@ -19,5 +19,8 @@ class PanDDA2Command:
                  local_cpus,
                  mem_per_core,
                  ):
-        self.command = f"python {analyse_path} {data_dirs} {out_dir} --pdb_regex='{pdb_regex}' --mtz_regex='{mtz_regex}' --structure_factors='(\"{structure_factors_f}\",\"{structure_factors_phi}\")' --autobuild={autobuild} --global_processing='{global_processing}' --local_cpus={local_cpus} --distributed_mem_per_core={mem_per_core} --scheduler={distributed_scheduler}"
+        self.command = f"python {analyse_path} {data_dirs} {out_dir} --pdb_regex='{pdb_regex}' --mtz_regex='" \
+                       f"{mtz_regex}' --structure_factors='(\"{structure_factors_f}\",\"{structure_factors_phi}\")' " \
+                       f"--autobuild={autobuild} --global_processing='{global_processing}' --local_cpus={local_cpus} " \
+                       f"--distributed_mem_per_core={mem_per_core} --distributed_scheduler={distributed_scheduler}"
 
