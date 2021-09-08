5e5dcc4b7926cabf180c13e1a92d964b240c4fc6
cwild <conor.wild@sky.com>
2021-09-08 14:35:34 +0100

    m

--- pandda_lib/command/pandda_2_command.py
+++ pandda_lib/command/pandda_2_command.py
@@ -22,5 +22,5 @@ class PanDDA2Command:
         self.command = f"python {analyse_path} {data_dirs} {out_dir} --pdb_regex='{pdb_regex}' --mtz_regex='" \
                        f"{mtz_regex}' --structure_factors='(\"{structure_factors_f}\",\"{structure_factors_phi}\")' " \
                        f"--autobuild={autobuild} --global_processing='{global_processing}' --local_cpus={local_cpus} " \
-                       f"--distributed_mem_per_core={mem_per_core} --distributed_scheduler={distributed_scheduler}"
+                       f"--distributed_mem_per_core={mem_per_core} --distributed_scheduler={distributed_scheduler}  --distributed_tmp=/data/share-2/conor/pandda/tmp"
 
