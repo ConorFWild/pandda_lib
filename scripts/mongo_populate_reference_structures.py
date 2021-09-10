46113b8b88ef10b0fb6f3d2cd47ae86bbce5da8b
cwild <conor.wild@sky.com>
2021-09-10 11:45:20 +0100

    m

--- scripts/mongo_populate_reference_structures.py
+++ scripts/mongo_populate_reference_structures.py
@@ -29,44 +29,48 @@ def main(reference_structure_dir: str):
     print(f"Reference scructure dir is: {reference_structure_dir}")
 
     for path in reference_structure_dir.glob("*"):
-        print(f"\tPath is: {path}")
-        dtag = Dtag.from_name(path.name)
-        print(f"\t\tDtag is {dtag}")
-        system_name = SystemName.from_dtag(dtag)
-        print(f"\t\tSystem is: {system_name}")
+        try:
+            print(f"\tPath is: {path}")
+            dtag = Dtag.from_name(path.name)
+            print(f"\t\tDtag is {dtag}")
+            system_name = SystemName.from_dtag(dtag)
+            print(f"\t\tSystem is: {system_name}")
 
-        mongo_system = pandda.System(system_name=system_name.system_name, )
-        mongo_system.save()
-        mongo_dataset = pandda.Dataset(dtag=dtag.dtag, system=mongo_system)
-        mongo_dataset.save()
+            mongo_system = pandda.System(system_name=system_name.system_name, )
+            mongo_system.save()
+            mongo_dataset = pandda.Dataset(dtag=dtag.dtag, system=mongo_system)
+            mongo_dataset.save()
 
-        # mongo_system = pandda.System.objects(system_name=system_name.system_name,)[0]
-        # mongo_dtag = pandda.Dataset.objects(dtag=dtag.dtag)[0]
-        #
+            # mongo_system = pandda.System.objects(system_name=system_name.system_name,)[0]
+            # mongo_dtag = pandda.Dataset.objects(dtag=dtag.dtag)[0]
+            #
 
-        structure = rmsd.Structure.from_path(path)
-        ligands = rmsd.Ligands.from_structure(structure)
+            structure = rmsd.Structure.from_path(path)
+            ligands = rmsd.Ligands.from_structure(structure)
 
-        mongo_ligands=[]
-        for ligand in ligands.structures:
-            centroid = ligand.centroid()
-            mongo_ligand = pandda.Ligand(
-                reference_model=None,
-                x=centroid[0],
-                y=centroid[1],
-                z=centroid[2],
+            mongo_ligands=[]
+            for ligand in ligands.structures:
+                centroid = ligand.centroid()
+                mongo_ligand = pandda.Ligand(
+                    reference_model=None,
+                    x=centroid[0],
+                    y=centroid[1],
+                    z=centroid[2],
+                )
+                mongo_ligand.save()
+                mongo_ligands.append(mongo_ligand)
+
+            mongo_reference_model = pandda.ReferenceModel(
+                path=str(path),
+                system=mongo_system,
+                dataset=mongo_dataset,
+                event=None,
+                ligands=mongo_ligands
             )
-            mongo_ligand.save()
-            mongo_ligands.append(mongo_ligand)
+            mongo_reference_model.save()
 
-        mongo_reference_model = pandda.ReferenceModel(
-            path=str(path),
-            system=mongo_system,
-            dataset=mongo_dataset,
-            event=None,
-            ligands=mongo_ligands
-        )
-        mongo_reference_model.save()
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     fire.Fire(main)
