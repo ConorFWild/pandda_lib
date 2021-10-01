dab1853b2dbc1355fbb6caeac887d3ebf7f69808
cwild <conor.wild@sky.com>
2021-10-01 13:46:48 +0100

    m

--- pandda_lib/rmsd/rmsd.py
+++ pandda_lib/rmsd/rmsd.py
@@ -68,7 +68,7 @@ class RMSD:
             for j, atom_1 in enumerate(res_1):
                 atom_2 = res_2[short_diff[j]]
 
-                assert atom_1.element.z == atom_2.element.z
+                assert atom_1.element.atomic_number == atom_2.element.atomic_number
 
                 distance = atom_1.pos.dist(atom_2.pos)
                 distances.append(distance)
