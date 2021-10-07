c7586af26abe2e941de4e3d25e918d9ecee5ffff
cwild <conor.wild@sky.com>
2021-10-07 10:54:14 +0100

    m

--- pandda_lib/command/pandda_1_command.py
+++ pandda_lib/command/pandda_1_command.py
@@ -7,39 +7,29 @@ from pathlib import Path
 @dataclass()
 class PanDDA1Command:
     def __init__(self,
-                 analyse_path,
                  data_dirs,
                  out_dir,
                  pdb_regex,
                  mtz_regex,
-                 structure_factors_f,
-                 structure_factors_phi,
-                 autobuild,
-                 global_processing,
-                 distributed_scheduler,
                  local_cpus,
-                 mem_per_core,
-                 distributed_tmp,
-                 rank_method,
-                 comparison_strategy,
-                 cluster_selection,
-                 num_workers,
+                 min_build_datasets,
+                 max_new_datasets,
                  log_file,
-
                  ):
         self.command = (
             f'pandda.analyse '
-            f'data_dirs={self.data_directory + "/*"} '
-            f'out_dir={self.panddas_directory} '
-            f'min_build_datasets={self.min_build_datasets} '
-            f'max_new_datasets={self.max_new_datasets} '
-            f'grid_spacing={self.grid_spacing} '
-            f'cpus={self.nproc} '
-            f'events.order_by={self.sort_event} '
-            f'pdb_style={self.pdb_style} '
-            f'mtz_style={self.mtz_style} '
+            f'data_dirs={data_dirs + "/*"} '
+            f'out_dir={out_dir} '
+            f'min_build_datasets={min_build_datasets} '
+            f'max_new_datasets={max_new_datasets} '
+            # f'grid_spacing={self.grid_spacing} '
+            f'cpus={local_cpus} '
+            # f'events.order_by={self.sort_event} '
+            f'pdb_style={pdb_regex} '
+            f'mtz_style={mtz_regex} '
             f'lig_style=/compound/*.cif '
-            f'apply_b_factor_scaling={self.wilson_scaling} '
-            f'write_average_map={self.write_mean_maps} '
-            f'average_map={self.calc_map_by} '
+            f'|& tee {log_file}'
+            # f'apply_b_factor_scaling={self.wilson_scaling} '
+            # f'write_average_map={self.write_mean_maps} '
+            # f'average_map={self.calc_map_by} '
         )
--- scripts/run_batch_pandda_1.py
+++ scripts/run_batch_pandda_1.py
@@ -7,24 +7,19 @@ from pandda_lib.distribution import ClusterHTCondor
 from pandda_lib import constants
 
 
-def main(analyse_path,
-         data_dirs,
-         pandda_dirs,
-         jobs=1,
-         main_cores_per_worker=6,
-         main_mem_per_core=50,
-         cores_per_worker=6,
-         mem_per_core=10,
-         num_workers=25,
-         pdb_regex="dimple.pdb",
-         mtz_regex="dimple.mtz",
-         structure_factors_f="FWT",
-         structure_factors_phi="PHWT",
-         autobuild=True,
-         global_processing="distributed",
-         distributed_scheduler="HTCONDOR",
-         distributed_tmp="/data/share-2/conor/pandda/tmp",
-         ):
+def main(
+        data_dirs,
+        pandda_dirs,
+        jobs=1,
+        main_cores_per_worker=6,
+        main_mem_per_core=50,
+        cores_per_worker=6,
+        pdb_regex="dimple.pdb",
+        mtz_regex="dimple.mtz",
+        min_build_datasets=30,
+        max_new_datasets=10000,
+        distributed_tmp="/data/share-2/conor/pandda/tmp",
+):
     data_dirs = Path(data_dirs)
     pandda_dirs = Path(pandda_dirs)
 
@@ -52,23 +47,13 @@ def main(analyse_path,
             TryMake(pandda_dir)()
 
             pandda_command = PanDDA1Command(
-                analyse_path=analyse_path,
                 data_dirs=data_dir,
                 out_dir=pandda_dir,
                 pdb_regex=pdb_regex,
                 mtz_regex=mtz_regex,
-                structure_factors_f=structure_factors_f,
-                structure_factors_phi=structure_factors_phi,
-                autobuild=autobuild,
-                global_processing=global_processing,
-                distributed_scheduler=distributed_scheduler,
                 local_cpus=cores_per_worker,
-                mem_per_core=mem_per_core,
-                distributed_tmp=distributed_tmp,
-                rank_method="autobuild",
-                comparison_strategy="high_res_random",
-                cluster_selection="close",
-                num_workers=num_workers,
+                min_build_datasets=min_build_datasets,
+                max_new_datasets=max_new_datasets,
                 log_file=pandda_dirs / f"{data_dir.name}.txt",
             )
             print(f"\tPanDDA command for {data_dir.name}: {pandda_command.command}")
@@ -79,6 +64,7 @@ def main(analyse_path,
             # callbacks.append(out_file.exists)
         else:
             print(f"\tEvent csv file: {out_file} already generated!")
+
 
 if __name__ == "__main__":
     fire.Fire(main)
