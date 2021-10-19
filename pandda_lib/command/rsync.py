19d2b0ac281e23ae3e83e671e428b9d57647adb5
cwild <conor.wild@sky.com>
2021-10-19 21:39:03 +0100

    m

--- pandda_lib/command/rsync.py
+++ pandda_lib/command/rsync.py
@@ -31,7 +31,41 @@ class RsyncPanDDADirs:
             )
         )
 
+
+command_aws: str = "sshpass -p '{password}' rsync --progress --exclude 'autoprocessing' --exclude 'dimple' --exclude " \
+                   "'jpg' --exclude '*.map' --exclude 'processed' --exclude '*.png' --exclude '*.pickle' --exclude " \
+                   "'*.jpg' -L -avzh -e ssh {path_to_local_dir} mx-static-highmem-htcondor-submit.diamond.ac.uk:{path_to_remote_dir}/"
+
+
+class RsyncPanDDADirsToAWS:
+    def __init__(self):
+        self.command: str = command_aws
+
+    def run(self):
+        p = subprocess.Popen(
+            self.command,
+            shell=True,
+        )
+
+        p.communicate()
+
+    @staticmethod
+    def from_paths(
+            path_to_remote_dir: Path,
+            path_to_local_dir: Path,
+            password: str,
+    ):
+        return RsyncPanDDADirs(
+            command.format(
+                path_to_remote_dir=path_to_remote_dir,
+                path_to_local_dir=path_to_local_dir,
+                password=password,
+            )
+        )
+
+
 command: str = "sshpass -p '{password}' rsync --progress -L -avzh -e ssh {path_to_local_dir} zoh22914@ssh.diamond.ac.uk:{path_to_remote_dir}/"
+
 
 @dataclass()
 class RsyncDirToDiamond:
---
+++ scripts/rsync_to_aws.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+import fire
+# import pymongo
+
+
+from pandda_lib import constants
+from pandda_lib.fs import XChemDiamondFS
+
+
+
+def main(diamond_dir: str, output_dir: str):
+    diamond_dir = Path(diamond_dir)
+    print(f"Diamond dir is: {diamond_dir}")
+
+    print("Getting XChemDiamondFS...")
+    diamond_fs = XChemDiamondFS.from_path(diamond_dir)
+
+    print(diamond_fs.pandda_dirs)
+
+    print(diamond_fs.model_building_dirs)
+
+    # print(",".join([str(x) for x in diamond_fs.model_building_dirs.values()]))
+    # print(",".join([str(",") for x in diamond_fs.pandda_dirs.values()]))
+
+    # client = pymongo.MongoClient()
+    # print(client.server_info())
+
+    # diamond_paths = client.pandda.diamond_paths
+    # diamond_paths.drop()
+
+    docs = []
+    for system_name, model_building_dir in diamond_fs.model_building_dirs.items():
+        doc = {
+                constants.mongo_diamond_paths_system_name: system_name.system_name,
+                constants.mongo_diamond_paths_model_building_dir: str(model_building_dir),
+                constants.mongo_diamond_paths_pandda_dirs: [str(x) for x in diamond_fs.pandda_dirs[system_name]],
+            }
+        print(
+            doc
+        )
+        docs.append(doc)
+
+    # diamond_paths.insert_many(docs)
+
+
+
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
