f14b5d86cbcd24d8a890364094a6da942299b489
cwild <conor.wild@sky.com>
2021-10-26 12:48:34 +0100

    m

--- scripts/mongo_populate_reference_structures_from_pandda_and_reference.py
+++ scripts/mongo_populate_reference_structures_from_pandda_and_reference.py
@@ -14,68 +14,72 @@ from pandda_lib import constants
 def main(reference_structure_dir:str, pandda_dir: str, table="pandda"):
     mongoengine.connect(table)
 
-    pandda_dir = Path(pandda_dir)
+    pandda_dirs = Path(pandda_dir)
 
-    print(f"Reference scructure dir is: {pandda_dir}")
+    print(f"PanDDA scructure dir is: {pandda_dir}")
 
-    processed_dataset_dirs = pandda_dir / constants.PANDDA_PROCESSED_DATASETS_DIR
+    for pandda_dir in pandda_dirs:
 
-    for processed_dataset_dir in processed_dataset_dirs.glob("*"):
+        processed_dataset_dirs = pandda_dir / constants.PANDDA_PROCESSED_DATASETS_DIR
 
-        try:
-            print(f"\tPath is: {processed_dataset_dir}")
-            dtag = Dtag.from_name(processed_dataset_dir.name)
-            print(f"\t\tDtag is {dtag}")
-            system_name = SystemName.from_dtag(dtag)
-            print(f"\t\tSystem is: {system_name}")
-
-            modelled_structure_path = processed_dataset_dir / constants.PANDDA_MODELLED_STRUCTURES_DIR / \
-                                      constants.PANDDA_EVENT_MODEL.format(dtag.dtag)
-
-            if not modelled_structure_path.exists():
-                raise Exception(f"No such model: {modelled_structure_path}")
+        for processed_dataset_dir in processed_dataset_dirs.glob("*"):
 
             try:
-                mongo_system = pandda.System.objects(system_name=system_name.system_name, )[0]
-            except Exception as e:
-                mongo_system = pandda.System(system_name=system_name.system_name, )
-                mongo_system.save()
+                print(f"\tPath is: {processed_dataset_dir}")
+                dtag = Dtag.from_name(processed_dataset_dir.name)
+                print(f"\t\tDtag is {dtag}")
+                system_name = SystemName.from_dtag(dtag)
+                print(f"\t\tSystem is: {system_name}")
 
-            try:
-                mongo_dataset = pandda.Dataset.objects(dtag=dtag.dtag)[0]
-            except Exception as e:
-                mongo_dataset = pandda.Dataset(dtag=dtag.dtag, system=mongo_system)
-                mongo_dataset.save()
+                modelled_structure_path = processed_dataset_dir / constants.PANDDA_MODELLED_STRUCTURES_DIR / \
+                                          constants.PANDDA_EVENT_MODEL.format(dtag.dtag)
 
-            structure = rmsd.Structure.from_path(modelled_structure_path)
-            ligands = rmsd.Ligands.from_structure(structure)
+                if not modelled_structure_path.exists():
+                    raise Exception(f"No such model: {modelled_structure_path}")
 
-            try:
-                mongo_reference_model = pandda.ReferenceModel(path=str(modelled_structure_path))[0]
-            except Exception as e:
-                mongo_ligands = []
-                for ligand in ligands.structures:
-                    centroid = ligand.centroid()
-                    mongo_ligand = pandda.Ligand(
-                        reference_model=None,
-                        x=centroid[0],
-                        y=centroid[1],
-                        z=centroid[2],
+                try:
+                    mongo_system = pandda.System.objects(system_name=system_name.system_name, )[0]
+                except Exception as e:
+                    mongo_system = pandda.System(system_name=system_name.system_name, )
+                    mongo_system.save()
+
+                try:
+                    mongo_dataset = pandda.Dataset.objects(dtag=dtag.dtag)[0]
+                except Exception as e:
+                    mongo_dataset = pandda.Dataset(dtag=dtag.dtag, system=mongo_system)
+                    mongo_dataset.save()
+
+                structure = rmsd.Structure.from_path(modelled_structure_path)
+                ligands = rmsd.Ligands.from_structure(structure)
+
+                try:
+                    mongo_reference_model = pandda.ReferenceModel(path=str(modelled_structure_path))[0]
+                except Exception as e:
+                    mongo_ligands = []
+                    for ligand in ligands.structures:
+                        centroid = ligand.centroid()
+                        mongo_ligand = pandda.Ligand(
+                            reference_model=None,
+                            x=centroid[0],
+                            y=centroid[1],
+                            z=centroid[2],
+                        )
+                        mongo_ligand.save()
+                        mongo_ligands.append(mongo_ligand)
+
+                    mongo_reference_model = pandda.ReferenceModel(
+                        path=str(modelled_structure_path.resolve()),
+                        system=mongo_system,
+                        dataset=mongo_dataset,
+                        event=None,
+                        ligands=mongo_ligands
                     )
-                    mongo_ligand.save()
-                    mongo_ligands.append(mongo_ligand)
+                    mongo_reference_model.save()
 
-                mongo_reference_model = pandda.ReferenceModel(
-                    path=str(modelled_structure_path.resolve()),
-                    system=mongo_system,
-                    dataset=mongo_dataset,
-                    event=None,
-                    ligands=mongo_ligands
-                )
-                mongo_reference_model.save()
+            except Exception as e:
+                print(e)
 
-        except Exception as e:
-            print(e)
+    ################
 
     reference_structure_dir = Path(reference_structure_dir)
 
