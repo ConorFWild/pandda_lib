ba70622f2c09d4ab89776450f6d5b46c75c66ee8
cwild <conor.wild@sky.com>
2021-11-01 14:03:59 +0000

    m

--- pandda_lib/fs/__init__.py
+++ pandda_lib/fs/__init__.py
@@ -1,2 +1,3 @@
 from pandda_lib.fs.pandda_dir import PanDDADir
 from pandda_lib.fs.diamond_fs import XChemDiamondFS
+from pandda_lib.fs.pandda_result import PanDDAsResult, PanDDAResult, DatasetResult, EventResult, BuildResult
