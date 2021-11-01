086a9f57c624b496ee394e260d8e21c0e937d7f2
cwild <conor.wild@sky.com>
2021-11-01 19:12:02 +0000

    m

--- scripts/structures_to_datasets.py
+++ scripts/structures_to_datasets.py
@@ -11,14 +11,15 @@ import fire
 def main(reference_structure_dir, reference_data_dir):
     for reference_structure_path in Path(reference_structure_dir).glob('*'):
         dtag = reference_structure_path.stem
-        print(dtag)
-        exit()
 
         reference_dataset_dir = Path(reference_data_dir) / dtag
         if reference_dataset_dir.exists():
+            print(f'N: Already have model for {dtag}')
+
             continue
 
         else:
+            print(f'Y: Transfering dtag: {dtag}')
             os.mkdir(reference_dataset_dir)
             new_reference_structure_path = reference_dataset_dir / 'final.pdb'
             shutil.copy(reference_structure_path, new_reference_structure_path)
