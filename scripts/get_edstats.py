b1704452842735a4b4d83d1a235f1efa31773fc8
cwild <conor.wild@sky.com>
2021-11-10 13:30:35 +0000

    p

--- scripts/get_edstats.py
+++ scripts/get_edstats.py
@@ -12,9 +12,17 @@ def main(data_dirs, mtz_regex="dimple.mtz", pdb_regex="dimple.pdb"):
 
     for data_dir in data_dirs.glob("*"):
         dtag = data_dir.name
+        print(f"Processing dtag: {dtag}")
 
         mtz_file = data_dir / mtz_regex
         pdb_file = data_dir / pdb_regex
+
+        if not mtz_file.exists():
+            print("\tSkipping! No mtz!")
+
+        if not pdb_file.exists():
+            print("\tSkipping! No pdb!")
+
 
         stats = EDSTATS(mtz_file, pdb_file)
 
