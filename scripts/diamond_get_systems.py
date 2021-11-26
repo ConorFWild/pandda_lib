fd4cd9763085f9e2e6acff8d6d29f5822a7ed4aa
cwild <conor.wild@sky.com>
2021-11-26 15:21:05 +0000

    p

--- scripts/diamond_get_systems.py
+++ scripts/diamond_get_systems.py
@@ -7,6 +7,8 @@ from pandda_lib.common import Dtag, SystemName
 def main():
     xchem_data_path = pathlib.Path('/dls/labxchem/data')
 
+    systems = []
+
     for year_dir in xchem_data_path.glob('*'):
         for project_dir in year_dir.glob('*'):
             model_building_dir = project_dir / 'processing' / 'analysis' / 'model_building'
@@ -25,11 +27,16 @@ def main():
                 dtag = Dtag.from_name(datasets_list[0].name)
                 system = SystemName.from_dtag(dtag)
 
-                print(f"{system.system_name}: {num_datasets}")
+                # print(f"{system.system_name}: {num_datasets}")
+
+                systems.append(f"{system.system_name}: {num_datasets}")
             except Exception as e:
                 # print(e)
                 continue
 
+
+    for x in sorted(systems):
+        print(x)
 
 if __name__ == "__main__":
     fire.Fire(main)
