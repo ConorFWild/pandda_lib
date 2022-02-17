3c4d06719f97589f3873cfde1b66e01be0bf086f
cwild <conor.wild@sky.com>
2022-02-17 13:52:07 +0000

    p

--- scripts/diamond_condor_submit_all_high_conf.py
+++ scripts/diamond_condor_submit_all_high_conf.py
@@ -2312,7 +2312,7 @@ def main(container_path: str):
         script = SCRIPT.format(
             data_dirs=str(data_dir),
             out_dir=str(out_dir),
-            only_datasets=str(system_high_confidence_dtags.join(","))
+            only_datasets=str(",".join(system_high_confidence_dtags))
         )
         print(f"\t\tScript is: {script}")
 
