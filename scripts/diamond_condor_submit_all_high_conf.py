40c67e154f59710e07fb55c24029f6493f716e2a
cwild <conor.wild@sky.com>
2022-03-02 11:13:42 +0000

    p

--- scripts/diamond_condor_submit_all_high_conf.py
+++ scripts/diamond_condor_submit_all_high_conf.py
@@ -51,7 +51,8 @@ echo "conda"
 conda activate pandda2 
 echo "pandda" 
 
-python -u /opt/clusterscratch/pandda/code/pandda_2_gemmi/pandda_gemmi/analyse.py --data_dirs={data_dirs} --out_dir={out_dir} --pdb_regex=\"dimple.pdb\" --mtz_regex=\"dimple.mtz\" --debug=True --only_datasets=\"{only_datasets}\"
+python -u /opt/clusterscratch/pandda/code/pandda_2_gemmi/pandda_gemmi/analyse.py --data_dirs={data_dirs} --out_dir={out_dir} --pdb_regex=\"dimple.pdb\" --mtz_regex=\"dimple.mtz\" --debug=True --only_datasets=\"{only_datasets}\" 
+--debug=True
 
 echo "done pandda" 
 """
