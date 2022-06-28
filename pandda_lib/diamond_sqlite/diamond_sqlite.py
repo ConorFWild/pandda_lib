62cb12b699aa676424182ab0fc5946069c38974e
cwild <conor.wild@sky.com>
2022-06-28 12:13:43 +0100

    p

--- pandda_lib/diamond_sqlite/diamond_sqlite.py
+++ pandda_lib/diamond_sqlite/diamond_sqlite.py
@@ -2,11 +2,14 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
+SYSTEMDATADIRSQLTABLE = "system_data_dir"
+
+
 Base = declarative_base()
 
 
-class SystemDataDir(Base):
-    __tablename__ = SYSTEMDATADIRTABLE
+class SystemDataDirSQL(Base):
+    __tablename__ = SYSTEMDATADIRSQLTABLE
 
     id = Column(Integer, primary_key=True)
     path = Column(String)
