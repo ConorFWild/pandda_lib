cedfd7c453b28cdf32e0670dc942ed270bcacfed
cwild <conor.wild@sky.com>
2022-06-28 17:08:03 +0100

    p

--- pandda_lib/schedulers/qsub_scheduler.py
+++ pandda_lib/schedulers/qsub_scheduler.py
@@ -1,6 +1,6 @@
 import subprocess
 
-submit_command = "qsub -pe smp 6 -l m_mem_free=30G -q medium.q {job_script_path}"
+submit_command = "qsub -pe smp 6 -l m_mem_free=30G -q medium -o {out_path} -e {err_path}.q {job_script_path}"
 
 
 class QSubScheduler:
@@ -15,8 +15,15 @@ class QSubScheduler:
         with open(job_script_path, 'w') as f:
             f.write(job.script)
 
+        out_path = self.tmp_dir / f"{job.name}.out"
+        err_path = self.tmp_dir / f"{job.name}.err"
+
         # Get submit command
-        _submit_command = submit_command.format(job_script_path=job_script_path)
+        _submit_command = submit_command.format(
+            job_script_path=job_script_path,
+            out_path=out_path,
+            err_path=err_path
+        )
 
         # Submit
         p = subprocess.Popen(
