eb9de03f8ad00a44472c287ed9ca6f79ca35d68a
cwild <conor.wild@sky.com>
2022-06-28 12:48:18 +0100

    p

--- scripts/database/diamond_build_input_data_database.py
+++ scripts/database/diamond_build_input_data_database.py
@@ -12,10 +12,12 @@ def __main__(sqlite_filepath):
 
     sqlite_filepath = pathlib.Path(sqlite_filepath).resolve()
     engine = create_engine(f"sqlite:///{str(sqlite_filepath)}")
-    session = sessionmaker(bind=engine)
+    session = sessionmaker(bind=engine)()
 
+    print("Getting diamond data dirs...")
     diamond_data_dirs = DiamondDataDirs()
 
+    print("Updating database...")
     for system in diamond_data_dirs:
         system_data_dir = diamond_data_dirs[system]
         system_data_dir_sql = SystemDataDirSQL(
@@ -25,6 +27,7 @@ def __main__(sqlite_filepath):
 
     session.commit()
 
+    print("Printing database...")
     for instance in session.query(SystemDataDirSQL).order_by(SystemDataDirSQL.id):
         print(instance.name, instance.fullname)
 
