a7feebf74a7afeb32107792372d7906ce76a6224
cwild <conor.wild@sky.com>
2022-06-29 13:11:25 +0100

    p

--- pandda_lib/diamond_sqlite/diamond_data.py
+++ pandda_lib/diamond_sqlite/diamond_data.py
@@ -54,7 +54,7 @@ class DiamondDataDirs:
                         data_dir_path = initial_model_dir
 
                     # num_datasets = len(datasets_list)
-                    dtags = [Dtag.from_name(datasets_list.name) for _dataset in datasets_list]
+                    dtags = [Dtag.from_name(_dataset.name) for _dataset in datasets_list]
                     system = max([
                         SystemName.from_dtag(dtag)
                         for dtag
