3fe94646746e178259e6151b0e86621b64e3e3e5
cwild <conor.wild@sky.com>
2022-07-13 17:02:57 +0100

    p

---
+++ scripts/fix_baz.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+import shutil
+
+import fire
+
+
+def fix_baz(baz_dir: str):
+    baz_dir = Path(baz_dir).resolve()
+
+    for dataset_dir in baz_dir.glob("*"):
+
+        compound_dir = dataset_dir / "compound"
+
+        # Make the directory for compounds
+        if not compound_dir.exists():
+            os.mkdir(compound_dir)
+
+        # Move the ligand pdb
+        ligand_pdb_path = dataset_dir / "ligand.pdb"
+        new_ligand_pdb_path = compound_dir
+        shutil.move(ligand_pdb_path, new_ligand_pdb_path)
+
+        # Move the ligand cif
+        ligand_cif_path = dataset_dir / "ligand.cif"
+        new_ligand_cif_path = compound_dir / "ligand.cif"
+        shutil.move(ligand_cif_path, new_ligand_cif_path)
+
+
+if __name__ == "__main__":
+    fire.Fire(fix_baz)
