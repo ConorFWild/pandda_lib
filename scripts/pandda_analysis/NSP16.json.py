c989b92bb887ad4a8963bae2c7a841f4ee2394a7
cwild <conor.wild@sky.com>
2022-08-03 18:01:51 +0100

    p

---
+++ scripts/pandda_analysis/NSP16.json.py
@@ -0,0 +1,7 @@
+{
+  "Dtags": ["NSP16-x0415", "MSP16-x0422"],
+  "Sample Points": {
+    "NSP16-x0415 conformational change": ["NSP16-x0415", 41.31, -58.02, -27.54],
+    "NSP16-x0422 partial ligand": ["NSP16-x0422", 80.61, -81.09, -0.65],
+  }
+}
