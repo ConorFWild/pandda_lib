58ec21650f97bf6063c685256ee97195127dc95a
cwild <conor.wild@sky.com>
2022-08-21 10:55:39 +0100

    p

---
+++ scripts/data_moving/ligands_to_compound_dirs.py
@@ -0,0 +1,24 @@
+import pathlib
+import os
+import shutil
+
+import fire
+
+def ligands_to_compound_dir(data_dir: str):
+    data_dir = pathlib.Path(data_dir)
+
+    for dataset_dir in data_dir.glob("*"):
+        # Make the compound dir
+        compound_dir = dataset_dir / "compound"
+        os.mkdir(compound_dir)
+
+        # Find the cifs in the dataset dir
+        cif_paths = list(dataset_dir.glob("*.cif"))
+
+        # Move them
+        new_ligand_path = compound_dir / "ligand.cif"
+        if len(cif_paths) > 0:
+            shutil.copy(cif_paths[0], new_ligand_path)
+
+if __name__ == "__main__":
+    fire.Fire(ligands_to_compound_dir)
