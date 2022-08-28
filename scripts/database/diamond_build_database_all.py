fc82d9b8df293e88fd8eb9b9dda0d53ba0441fd2
cwild <conor.wild@sky.com>
2022-08-28 18:40:29 +0100

    p

--- scripts/database/diamond_build_database_all.py
+++ scripts/database/diamond_build_database_all.py
@@ -13,10 +13,11 @@ from diamond_add_fragalysis_reference_structures import diamond_add_fragalysis_r
 from diamond_add_model_stats import diamond_add_model_stats
 
 
-def diamond_build_database_all(sqlite_filepath, reference_structure_dir):
+def diamond_build_database_all(sqlite_filepath, reference_structure_dir, tmp_dir):
     diamond_build_input_data_database(sqlite_filepath)
     database_add_diamond_panddas(sqlite_filepath)
     diamond_add_fragalysis_reference_structures(sqlite_filepath, reference_structure_dir)
+    diamond_add_model_stats(sqlite_filepath, tmp_dir)
 
 if __name__ == "__main__":
     fire.Fire(diamond_build_database_all)
