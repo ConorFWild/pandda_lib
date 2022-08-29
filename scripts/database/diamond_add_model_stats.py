9c15c5d0cc98e3f251846d5a372358615abc85ae
cwild <conor.wild@sky.com>
2022-08-29 10:58:52 +0100

    p

--- scripts/database/diamond_add_model_stats.py
+++ scripts/database/diamond_add_model_stats.py
@@ -22,46 +22,49 @@ from pandda_lib.rscc import get_rscc
 
 
 def get_dataset_rsccs(dataset_dtag, dataset_path, dataset_bound_state_model_path, event_maps, mtz_path, tmp_dir):
-    dataset_dtag = dataset_dtag
-    dataset_path = pathlib.Path(dataset_path)
-    dataset_bound_state_model_path = pathlib.Path(dataset_bound_state_model_path)
+    try:
+        dataset_dtag = dataset_dtag
+        dataset_path = pathlib.Path(dataset_path)
+        dataset_bound_state_model_path = pathlib.Path(dataset_bound_state_model_path)
 
-    if not tmp_dir.exists():
-        os.mkdir(tmp_dir)
+        if not tmp_dir.exists():
+            os.mkdir(tmp_dir)
 
-    # event_maps = dataset.event_maps
-    resolution = gemmi.read_mtz_file(mtz_path).resolution_high()
+        # event_maps = dataset.event_maps
+        resolution = gemmi.read_mtz_file(mtz_path).resolution_high()
 
-    rsccs = {}
-    for event_map in event_maps:
-        rscc = get_rscc(
-            dataset_bound_state_model_path,
-            event_map.path,
-            resolution,
-            tmp_dir
-        )
-        for chain_res, _rscc in rscc:
-            # rsccs.append(_rscc)
-            rsccs[(event_map.path, chain_res[0], chain_res[1])] = _rscc
+        rsccs = {}
+        for event_map in event_maps:
+            rscc = get_rscc(
+                dataset_bound_state_model_path,
+                event_map.path,
+                resolution,
+                tmp_dir
+            )
+            for chain_res, _rscc in rscc:
+                # rsccs.append(_rscc)
+                rsccs[(event_map.path, chain_res[0], chain_res[1])] = _rscc
 
-    # # Get the custom scores
-    # custom_scores = {}
-    # for event_map in event_maps:
-    #     custom_score = get_custom_score(
-    #         dataset_bound_state_model_path,
-    #         event_map.path,
-    #         resolution,
-    #     )
-    #     for chain_res, _custom_score in custom_score:
-    #     #     custom_scores.append(_custom_score)
-    #         custom_scores[(event_map.path, chain_res[0], chain_res[1])] = _custom_score
+        # # Get the custom scores
+        # custom_scores = {}
+        # for event_map in event_maps:
+        #     custom_score = get_custom_score(
+        #         dataset_bound_state_model_path,
+        #         event_map.path,
+        #         resolution,
+        #     )
+        #     for chain_res, _custom_score in custom_score:
+        #     #     custom_scores.append(_custom_score)
+        #         custom_scores[(event_map.path, chain_res[0], chain_res[1])] = _custom_score
 
-    # selected_rscc_index = np.argmax(rsccs)
-    selected_rscc_id = max(rsccs, key=lambda _key: rsccs[_key])
-    selected_rscc = rsccs[selected_rscc_id]
+        # selected_rscc_index = np.argmax(rsccs)
+        selected_rscc_id = max(rsccs, key=lambda _key: rsccs[_key])
+        selected_rscc = rsccs[selected_rscc_id]
 
-    return selected_rscc
+        return selected_rscc
 
+    except Exception as e:
+        raise Exception(f"Exception occured for dtag {dataset_dtag} model {dataset_bound_state_model_path}:\n{str(e)}")
 
 def diamond_add_model_stats(sqlite_filepath, tmp_dir):
     sqlite_filepath = pathlib.Path(sqlite_filepath).resolve()
