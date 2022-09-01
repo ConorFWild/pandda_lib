f070655c734daa371eb48fb5998f12f1a99f4960
cwild <conor.wild@sky.com>
2022-09-01 14:06:16 +0100

    p

--- pandda_lib/diamond_sqlite/diamond_sqlite.py
+++ pandda_lib/diamond_sqlite/diamond_sqlite.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table, Float, Bool
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, Float, Boolean
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
