f01b5aa1f8fa82e7fb6238ad47252e3f95007d59
cwild <conor.wild@sky.com>
2022-09-01 11:44:33 +0100

    p

--- scripts/pandda_autobuild/add_autobuild_panddas_to_sql.py
+++ scripts/pandda_autobuild/add_autobuild_panddas_to_sql.py
@@ -41,11 +41,11 @@ def database_add_diamond_panddas(sqlite_filepath, pandda_autobuilds_dir):
     for system in systems:
         for project in system.projects:
             system_project_dir = pandda_autobuilds_dir / f"system_{system.system_name}_project_{project.project_name}"
-            try:
-                pandda_dir_sql = get_pandda_2_result(system_project_dir, system, project)
-                session.add(pandda_dir_sql)
-            except Exception as e:
-                print(f"\tCouldn't get PanDDA for {system_project_dir}")
+            # try:
+            pandda_dir_sql = get_pandda_2_result(system_project_dir, system, project)
+            session.add(pandda_dir_sql)
+            # except Exception as e:
+            #     print(f"\tCouldn't get PanDDA for {system_project_dir}")
 
     session.commit()
 
