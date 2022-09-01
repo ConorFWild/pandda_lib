2f87f78a257234ce9923612737dbd837d468bc80
cwild <conor.wild@sky.com>
2022-09-01 11:27:53 +0100

    p

--- pandda_lib/fs/pandda_result.py
+++ pandda_lib/fs/pandda_result.py
@@ -158,12 +158,12 @@ class DatasetResult:
         events = {}
         for event_dir in processed_dataset_dir.glob('*'):
             if re.match('[0-9]+', event_dir.name):
-                try:
-                    event_result = EventResult.from_dir(event_dir, event_table)
-                    events[event_dir.name] = event_result
-                except Exception as e:
-                    print(f"\t{event_dir}: {e}")
-                    continue
+                # try:
+                event_result = EventResult.from_dir(event_dir, event_table)
+                events[event_dir.name] = event_result
+                # except Exception as e:
+                #     print(f"\t{event_dir}: {e}")
+                #     continue
 
         return DatasetResult(
             path,
