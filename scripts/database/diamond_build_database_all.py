2f2fd571566e24560bbf1dfe721d8f898ef64118
cwild <conor.wild@sky.com>
2022-09-02 11:04:05 +0100

    p

--- scripts/database/diamond_build_database_all.py
+++ scripts/database/diamond_build_database_all.py
@@ -25,17 +25,17 @@ def diamond_build_database_all(options_json="database_options.json", step=0):
     tmp_dir = options["tmp_dir"]
     pandda_autobuilds_dir = options["pandda_autobuilds_dir"]
 
-    if step < 1:
+    if step <= 1:
         diamond_build_input_data_database(sqlite_filepath)
-    if step < 2:
+    if step <= 2:
         database_add_diamond_panddas(sqlite_filepath)
-    if step < 3:
+    if step <= 3:
         diamond_add_fragalysis_reference_structures(sqlite_filepath, reference_structure_dir)
-    if step < 4:
+    if step <= 4:
         diamond_add_model_stats(sqlite_filepath, tmp_dir)
-    if step < 5:
+    if step <= 5:
         database_add_autobuild_panddas(sqlite_filepath, pandda_autobuilds_dir)
-    if step < 6:
+    if step <= 6:
         diamond_add_autobuild_rmsds(sqlite_filepath)
 
 
