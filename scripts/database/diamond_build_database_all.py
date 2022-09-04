6b4be3277c3e419a4754419fd78ca9496c71f640
cwild <conor.wild@sky.com>
2022-09-04 18:05:10 +0100

    p

--- scripts/database/diamond_build_database_all.py
+++ scripts/database/diamond_build_database_all.py
@@ -46,5 +46,6 @@ def diamond_build_database_all(options_json="database_options.json", step=0, cpu
     if step <= 7:
         diamond_add_autobuild_rmsds(sqlite_filepath, cpus=3)
 
+
 if __name__ == "__main__":
     fire.Fire(diamond_build_database_all)
