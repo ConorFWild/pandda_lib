7b557561d6cce14f05607437b711af2e51a1523e
cwild <conor.wild@sky.com>
2022-09-13 11:36:52 +0100

    p

---
+++ scripts/figures/plot_all_figures.py
@@ -0,0 +1,34 @@
+import pathlib
+import os
+
+import numpy as np
+import pandas as pd
+
+import fire
+from sqlalchemy.orm import sessionmaker, subqueryload
+from sqlalchemy import create_engine
+
+from pandda_lib import constants
+from pandda_lib.diamond_sqlite.diamond_data import DiamondDataDirs
+from pandda_lib.fs.pandda_result import PanDDAResult
+from pandda_lib.diamond_sqlite.diamond_sqlite import *
+
+import gemmi
+
+import seaborn as sns
+
+sns.set(rc={'figure.figsize': (2 * 11.7, 2 * 8.27)})
+sns.set(font_scale=3)
+# sns.color_palette("hls", 8)
+sns.set_palette("hls")
+sns.set_palette("crest")
+
+from plot_rscc_vs_rmsd import plot_rscc_vs_rmsd
+
+
+def plot_all_figures():
+    plot_rscc_vs_rmsd()
+
+
+if __name__ == "__main__":
+    fire.Fire(plot_all_figures())
