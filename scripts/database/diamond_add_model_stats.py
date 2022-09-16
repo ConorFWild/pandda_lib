ecd69f6f53bed2ff26ce3e984ff975266b270e87
cwild <conor.wild@sky.com>
2022-09-16 21:34:56 +0100

    p

--- scripts/database/diamond_add_model_stats.py
+++ scripts/database/diamond_add_model_stats.py
@@ -44,59 +44,59 @@ class GetDatasetRSCC:
 
 
 def get_dataset_rsccs(dataset_dtag, dataset_path, dataset_bound_state_model_path, event_maps, mtz_path, tmp_dir):
-    try:
-        dataset_dtag = dataset_dtag
-        dataset_path = pathlib.Path(dataset_path)
-        dataset_bound_state_model_path = pathlib.Path(dataset_bound_state_model_path)
+    # try:
+    dataset_dtag = dataset_dtag
+    dataset_path = pathlib.Path(dataset_path)
+    dataset_bound_state_model_path = pathlib.Path(dataset_bound_state_model_path)
 
-        if not tmp_dir.exists():
-            os.mkdir(tmp_dir)
+    if not tmp_dir.exists():
+        os.mkdir(tmp_dir)
 
-        # event_maps = dataset.event_maps
-        if not pathlib.Path(mtz_path).exists():
-            return None
-        resolution = gemmi.read_mtz_file(mtz_path).resolution_high()
+    # event_maps = dataset.event_maps
+    if not pathlib.Path(mtz_path).exists():
+        return None
+    resolution = gemmi.read_mtz_file(mtz_path).resolution_high()
 
-        rsccs = {}
-        for event_map in event_maps:
-            rscc = get_rscc(
-                dataset_bound_state_model_path,
-                event_map.path,
-                resolution,
-                tmp_dir
-            )
-            print(rscc)
+    rsccs = {}
+    for event_map in event_maps:
+        rscc = get_rscc(
+            dataset_bound_state_model_path,
+            event_map.path,
+            resolution,
+            tmp_dir
+        )
+        print(rscc)
 
-            if not rscc:
-                continue
+        if not rscc:
+            continue
 
-            for chain_res, _rscc in rscc.items():
-                # rsccs.append(_rscc)
-                rsccs[(event_map.path, chain_res[0], chain_res[1])] = _rscc
+        for chain_res, _rscc in rscc.items():
+            # rsccs.append(_rscc)
+            rsccs[(event_map.path, chain_res[0], chain_res[1])] = _rscc
 
-        # # Get the custom scores
-        # custom_scores = {}
-        # for event_map in event_maps:
-        #     custom_score = get_custom_score(
-        #         dataset_bound_state_model_path,
-        #         event_map.path,
-        #         resolution,
-        #     )
-        #     for chain_res, _custom_score in custom_score:
-        #     #     custom_scores.append(_custom_score)
-        #         custom_scores[(event_map.path, chain_res[0], chain_res[1])] = _custom_score
+    # # Get the custom scores
+    # custom_scores = {}
+    # for event_map in event_maps:
+    #     custom_score = get_custom_score(
+    #         dataset_bound_state_model_path,
+    #         event_map.path,
+    #         resolution,
+    #     )
+    #     for chain_res, _custom_score in custom_score:
+    #     #     custom_scores.append(_custom_score)
+    #         custom_scores[(event_map.path, chain_res[0], chain_res[1])] = _custom_score
 
-        if len(rsccs) == 0:
-            return None
+    if len(rsccs) == 0:
+        return None
 
-        # selected_rscc_index = np.argmax(rsccs)
-        selected_rscc_id = max(rsccs, key=lambda _key: rsccs[_key])
-        selected_rscc = rsccs[selected_rscc_id]
+    # selected_rscc_index = np.argmax(rsccs)
+    selected_rscc_id = max(rsccs, key=lambda _key: rsccs[_key])
+    selected_rscc = rsccs[selected_rscc_id]
 
-        return selected_rscc
+    return selected_rscc
 
-    except Exception as e:
-        raise Exception(f"Exception occured for dtag {dataset_dtag} model {dataset_bound_state_model_path}:\n{str(e)}")
+    # except Exception as e:
+    #     raise Exception(f"Exception occured for dtag {dataset_dtag} model {dataset_bound_state_model_path}:\n{str(e)}")
 
 
 def get_run_set(datasets, tmp_dir):
@@ -175,12 +175,19 @@ def diamond_add_model_stats(sqlite_filepath, tmp_dir, cpus=3):
         print("Running")
         selected_rsccs_iterable = p.imap(
             Runner(),
-            get_run_set(datasets, tmp_dir),
-            chunksize=10,
+            get_run_set(
+                datasets,
+                tmp_dir,
+            ),
+            chunksize=1,
         )
 
         print(f"Getting results...")
-        selected_rsccs = [rscc for rscc in selected_rsccs_iterable]
+        selected_rsccs = []
+
+        for rscc in selected_rsccs_iterable:
+            print(f"RSCC: {rscc}")
+            selected_rsccs.append(rscc)
 
     print("Inserting to database...")
     for dataset, selected_rscc in zip(datasets, selected_rsccs):
