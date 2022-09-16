21dee7f1a6d75d02809df3f8ae9e48884c73cef3
cwild <conor.wild@sky.com>
2022-09-16 21:37:28 +0100

    p

--- scripts/database/diamond_add_model_stats.py
+++ scripts/database/diamond_add_model_stats.py
@@ -10,7 +10,6 @@ from sqlalchemy import create_engine
 # from joblib import Parallel, delayed
 import multiprocessing as mp
 
-
 from pandda_lib import constants
 from pandda_lib.diamond_sqlite.diamond_data import DiamondDataDirs
 from pandda_lib.fs.pandda_result import PanDDAResult
@@ -102,65 +101,66 @@ def get_dataset_rsccs(dataset_dtag, dataset_path, dataset_bound_state_model_path
 def get_run_set(datasets, tmp_dir):
     for dataset in datasets:
         yield GetDatasetRSCC(dataset.dtag,
-                       dataset.path,
-                       dataset.pandda_model_path,
-                       dataset.event_maps,
-                       dataset.mtz_path,
-                       tmp_dir / dataset.dtag)
+                             dataset.path,
+                             dataset.pandda_model_path,
+                             dataset.event_maps,
+                             dataset.mtz_path,
+                             tmp_dir / dataset.dtag)
 
 
 def diamond_add_model_stats(sqlite_filepath, tmp_dir, cpus=3):
-    sqlite_filepath = pathlib.Path(sqlite_filepath).resolve()
-    tmp_dir = pathlib.Path(tmp_dir).resolve()
-    engine = create_engine(f"sqlite:///{str(sqlite_filepath)}")
-    session = sessionmaker(bind=engine)()
-    Base.metadata.create_all(engine)
-
-    # Remove tables
-    Base.metadata.create_all(engine)
-    BoundStateModelSQL.__table__.drop(engine)
-    Base.metadata.create_all(engine)
-
-    initial_datasets = session.query(DatasetSQL).options(subqueryload(DatasetSQL.event_maps)).order_by(
-        DatasetSQL.id).all()
-    print(len(initial_datasets))
-
-    print("Updating database...")
-    datasets = []
-    datasets_without_pandda_models = []
-    # for system in systems:
-    #     for project in system.projects:
-    for dataset in initial_datasets:
-        print(dataset.pandda_model_path)
-        if dataset.pandda_model_path != "None":
-            datasets.append(dataset)
-        else:
-            datasets_without_pandda_models.append(dataset)
-            # selected_custom_score = custom_scores[selected_rscc_id]
-    print(f"\tNumber of datasets to score: {len(datasets)}; number not to: {len(datasets_without_pandda_models)}")
-
-    print("Getting RSCCs...")
-    # selected_rsccs = Parallel(
-    #     n_jobs=24,
-    #     verbose=50,
-    # )(
-    #     delayed(get_dataset_rsccs)(
-    #         dataset.dtag,
-    #         dataset.path,
-    #         dataset.pandda_model_path,
-    #         dataset.event_maps,
-    #         dataset.mtz_path,
-    #         tmp_dir / dataset.dtag
-    #     )
-    #     for dataset
-    #     in datasets
-    # )
     try:
         mp.set_start_method('spawn')
     except Exception as e:
         print(e)
 
     with mp.Pool(cpus) as p:
+        sqlite_filepath = pathlib.Path(sqlite_filepath).resolve()
+        tmp_dir = pathlib.Path(tmp_dir).resolve()
+        engine = create_engine(f"sqlite:///{str(sqlite_filepath)}")
+        session = sessionmaker(bind=engine)()
+        Base.metadata.create_all(engine)
+
+        # Remove tables
+        Base.metadata.create_all(engine)
+        BoundStateModelSQL.__table__.drop(engine)
+        Base.metadata.create_all(engine)
+
+        initial_datasets = session.query(DatasetSQL).options(subqueryload(DatasetSQL.event_maps)).order_by(
+            DatasetSQL.id).all()
+        print(len(initial_datasets))
+
+        print("Updating database...")
+        datasets = []
+        datasets_without_pandda_models = []
+        # for system in systems:
+        #     for project in system.projects:
+        for dataset in initial_datasets:
+            print(dataset.pandda_model_path)
+            if dataset.pandda_model_path != "None":
+                datasets.append(dataset)
+            else:
+                datasets_without_pandda_models.append(dataset)
+                # selected_custom_score = custom_scores[selected_rscc_id]
+        print(f"\tNumber of datasets to score: {len(datasets)}; number not to: {len(datasets_without_pandda_models)}")
+
+        print("Getting RSCCs...")
+        # selected_rsccs = Parallel(
+        #     n_jobs=24,
+        #     verbose=50,
+        # )(
+        #     delayed(get_dataset_rsccs)(
+        #         dataset.dtag,
+        #         dataset.path,
+        #         dataset.pandda_model_path,
+        #         dataset.event_maps,
+        #         dataset.mtz_path,
+        #         tmp_dir / dataset.dtag
+        #     )
+        #     for dataset
+        #     in datasets
+        # )
+
         print("Getting run set")
         # run_set = [
         #     GetDatasetRSCC(dataset.dtag,
