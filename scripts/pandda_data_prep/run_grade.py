02e99b0bb45f6609e583a8d5d78300f797adb208
jfi22496 <conor.wild@sky.com>
2023-02-06 13:48:38 +0000

    Grade command

--- scripts/pandda_data_prep/run_grade.py
+++ scripts/pandda_data_prep/run_grade.py
@@ -3,9 +3,23 @@ from pathlib import Path
 import subprocess
 
 import fire
+import joblib
 
 grade_command = "module load buster; cd {data_dir}; grade -in {in_smiles} -ocif {out_cif}"
 
+
+def run_grade(compound_dir, smiles_path):
+    p = subprocess.Popen(
+        grade_command.format(
+            data_dir=compound_dir,
+            in_smiles=smiles_path,
+            out_cif=f"{smiles_path.root}.cif",
+        ),
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    p.communicate()
 
 def run_grade_on_model_building(path: str):
 
@@ -24,19 +38,10 @@ def run_grade_on_model_building(path: str):
             continue
 
         processes.append(
-            subprocess.Popen(
-                grade_command.format(
-                    data_dir=compound_dir,
-                    in_smiles=smiles_path,
-                    out_cif=f"{smiles_path.root}.cif",
-                ),
-                shell=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
-            )
+            joblib.delayed(run_grade)(compound_dir, smiles_path)
         )
 
-    [p.communicate() for p in processes]
+    joblib.Parallel(n_jobs=12)(x for x in processes)
 
 if __name__ == "__main__":
     fire.Fire(run_grade_on_model_building)
