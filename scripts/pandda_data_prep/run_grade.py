21f2c141636b83996442359dbb10797849a6fe27
jfi22496 <conor.wild@sky.com>
2023-02-14 09:05:06 +0000

    Grade command

--- scripts/pandda_data_prep/run_grade.py
+++ scripts/pandda_data_prep/run_grade.py
@@ -8,6 +8,22 @@ import joblib
 
 grade_command = "module load buster; module load buster; cd {data_dir}; grade -checkdeps; grade -f -in {in_smiles} -ocif {out_cif}"
 
+def run_grade_cif(compound_dir, cif_path):
+    command = grade_command.format(
+        data_dir=compound_dir,
+        in_smiles=cif_path,
+        out_cif=cif_path,
+    )
+    print(command)
+    p = subprocess.Popen(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    stdout, stderr = p.communicate()
+    print(str(stdout))
+    print(str(stderr))
 
 def run_grade(compound_dir, smiles_path):
     print(f"Running job: {compound_dir} {smiles_path.name}")
@@ -38,33 +54,48 @@ def run_grade(compound_dir, smiles_path):
         print(str(stderr))
 
 
-def run_grade_on_model_building(path: str, remove=False):
-    _path = Path(path).resolve()
-    processes = []
-    for model_dir in _path.glob("*"):
-        compound_dir = model_dir / "compound"
-        skip = False
-        for cif in compound_dir.glob("*.cif"):
-            if remove:
-                os.remove(cif)
-                print(f"Removed cif: {cif}")
-            else:
-                skip = True
-        if skip:
-            print(f"Already has cif: {compound_dir}: skipping!")
-            continue
-        try:
-            smiles_path = next(compound_dir.glob("*.smiles"))
-        except:
-            print(f"No smiles in dir: {compound_dir}")
-            continue
+class Grade:
+    def run_grade_on_model_building(self, path: str, remove=False,):
+        _path = Path(path).resolve()
+        processes = []
+        for model_dir in _path.glob("*"):
+            compound_dir = model_dir / "compound"
+            skip = False
+            for cif in compound_dir.glob("*.cif"):
+                if remove:
+                    os.remove(cif)
+                    print(f"Removed cif: {cif}")
+                else:
+                    skip = True
+            if skip:
+                print(f"Already has cif: {compound_dir}: skipping!")
+                continue
+            try:
+                smiles_path = next(compound_dir.glob("*.smiles"))
+            except:
+                print(f"No smiles in dir: {compound_dir}")
+                continue
 
-        processes.append(
-            joblib.delayed(run_grade)(compound_dir, smiles_path)
-        )
-    print(f"Processing {len(processes)} jobs...")
-    joblib.Parallel(n_jobs=12, verbose=10)(x for x in processes)
+            processes.append(
+                joblib.delayed(run_grade)(compound_dir, smiles_path)
+            )
+        print(f"Processing {len(processes)} jobs...")
+        joblib.Parallel(n_jobs=12, verbose=10)(x for x in processes)
+
+    def run_grade_on_model_building_cifs(self, path: str):
+        _path = Path(path).resolve()
+        processes = []
+        for model_dir in _path.glob("*"):
+            compound_dir = model_dir / "compound"
+            skip = False
+            for cif in compound_dir.glob("*.cif"):
+
+                processes.append(
+                    joblib.delayed(run_grade)(compound_dir, cif)
+                )
+        print(f"Processing {len(processes)} jobs...")
+        joblib.Parallel(n_jobs=12, verbose=10)(x for x in processes)
 
 
 if __name__ == "__main__":
-    fire.Fire(run_grade_on_model_building)
+    fire.Fire(Grade)
