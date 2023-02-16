2a5add2b1a72660f16a2734fbe1067b990623329
jfi22496 <conor.wild@sky.com>
2023-02-16 10:24:47 +0000

    Autobuild rank score default

--- pandda_lib/jobs/pandda_job.py
+++ pandda_lib/jobs/pandda_job.py
@@ -54,7 +54,7 @@ class PanDDAJob:
                  comparison_strategy="hybrid",
                  event_score="inbuilt",
                  autobuild="False",
-                 rank_method="event_score",
+                 rank_method="autobuild_score",
                  memory_availability="low",
                  local_processing="serial",
                  global_processing="serial",
