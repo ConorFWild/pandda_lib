bfbd85bf0584af783b1b69b1fd615da4f48223be
jfi22496 <conor.wild@sky.com>
2023-05-23 13:07:53 +0100

    Autobuild rank score default

--- scripts/thesis/event_map_rscc_annotation/generate_fake_pandda_for_annotation.py
+++ scripts/thesis/event_map_rscc_annotation/generate_fake_pandda_for_annotation.py
@@ -2,8 +2,8 @@ import pathlib
 import os
 
 import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
+# import pandas as pd
+# from matplotlib import pyplot as plt
 
 import fire
 from sqlalchemy.orm import sessionmaker, subqueryload
