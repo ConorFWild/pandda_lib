fdb44706b0a977995661099082368e05919e27ac
jfi22496 <conor.wild@sky.com>
2023-05-24 21:51:03 +0100

    Autobuild rank score default

--- scripts/thesis/poorly_ranked_binder_sample/make_fake_pandda_for_high_ranking_non_hits.py
+++ scripts/thesis/poorly_ranked_binder_sample/make_fake_pandda_for_high_ranking_non_hits.py
@@ -207,7 +207,7 @@ def plot_rscc_vs_rmsd():
             continue
 
         inspect_table = pd.read_csv(inspect_table_path)
-        inspect_table.sort(by="cluster_size", inplace=True, ignore_index=True)
+        inspect_table.sort(by="cluster_size", inplace=True, ignore_index=True, ascending=False)
 
         # Check if there are high confidence modelled hits
         high_conf_event_table = inspect_table[
