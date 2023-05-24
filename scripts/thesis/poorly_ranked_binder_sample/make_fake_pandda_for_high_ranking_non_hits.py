e44f64f608a75a9e903d5b9944c911dae995b790
jfi22496 <conor.wild@sky.com>
2023-05-24 21:42:08 +0100

    Autobuild rank score default

--- scripts/thesis/poorly_ranked_binder_sample/make_fake_pandda_for_high_ranking_non_hits.py
+++ scripts/thesis/poorly_ranked_binder_sample/make_fake_pandda_for_high_ranking_non_hits.py
@@ -220,6 +220,7 @@ def plot_rscc_vs_rmsd():
 
         # Lowest ranking high conf dataset
         lowest_ranked_high_conf_index = high_conf_event_table.index.values[-1]
+        print(f"Lowest ranked high confidence hit: {lowest_ranked_high_conf_index}")
         truncated_event_table = inspect_table.loc[:lowest_ranked_high_conf_index]
 
         # If so, get highest ranked non-hit, and up to 5 random non-hits up to lowest ranked high confidence, placed lig
