6447f8119cb6dacbb96bd50950941c660e46bec3
jfi22496 <conor.wild@sky.com>
2023-05-25 12:24:53 +0100

    Autobuild rank score default

--- scripts/thesis/poorly_ranked_binder_sample/plot_high_ranking_non_hits.py
+++ scripts/thesis/poorly_ranked_binder_sample/plot_high_ranking_non_hits.py
@@ -14,3 +14,25 @@
     12: "Water Repositioning",  # Clear relocation of water sized blob
     13: "Data Viewing Error"  # Data failed to open
 }
+
+{
+    # Protein Dependent
+    0: "Low Resolution Smoothing",  # Data with a low resolution in same conf. as ground state
+    1: "Disorder",  # Missing protein density
+    2: "Conformational Change",  # A Clear conformational change relative to the ground state map
+    3: "Interface Conformational Change",  # A Clear conformational change relative to the ground state map
+    # Solvent Dependent
+    4: "High Resolution Blob",  # A large isolated blob too big to be water and too small to be a ligand
+    # Protein-Solvent Dependent
+    5: "Poor Data Quality",  # Broadly uninterpretable event map
+    6: "Misannotated Hit",  # A clear fragment binding event
+    7: "Contaminent Binding",  # Interpretable, non-ligand density
+    8: "Interface Artefact",  # A blob at a crystal contact
+    9: "Water Repositioning",  # Clear relocation of water sized blob
+    10: "Localized Uninterpretable Density",  # Non-isolated blob
+    # Algorithm Dependent
+    11: "Blob far from Protein",  # A blob far from protein density
+    12: "Data Viewing Error",  # Data failed to open
+    13: "Merging of Unrelated Blobs",  # Deprecated
+    14: "Cofactor Orientation Change",  # A change in the position of a cofactor
+}
