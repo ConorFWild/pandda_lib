ec2191d8377c7fef304cdbae5bebe2415fa4c721
jfi22496 <conor.wild@sky.com>
2023-05-30 23:26:28 +0100

    Autobuild rank score default

--- scripts/thesis/xchem_dataset_summary/plot_xchem_dataset_summary.py
+++ scripts/thesis/xchem_dataset_summary/plot_xchem_dataset_summary.py
@@ -83,6 +83,9 @@ def plot_xchem_dataset_summaries():
         for ds in system_datasets.values():
             if pathlib.Path(ds.mtz_path).exists():
                 system_dataset_resolutions.append(gemmi.read_mtz_file(ds.mtz_path).resolution_high())
+
+        if len(system_dataset_resolutions) == 0:
+            continue
         min_res = min(system_dataset_resolutions)
 
         # Get mean resolution
