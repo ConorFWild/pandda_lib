3c2ddf1796970242c06f4ffa30913aa466ffae58
jfi22496 <conor.wild@sky.com>
2023-06-04 23:41:43 +0100

    Autobuild rank score default

--- scripts/thesis/molecule_figures/make_molecule_figures_fake_pandda.py
+++ scripts/thesis/molecule_figures/make_molecule_figures_fake_pandda.py
@@ -29,10 +29,10 @@ molecules = {
                 ("AAVNAR-x1501", 1), ("NSP16-x0155", 4)],
     "LowRes": [("AAVNAR-x1660", 1), ("70X-x0090", 3), ("STAG1A-x0535", 5)],
     "Hetro": [("NSP16-x0440", 2), ("STAG1A-x0452", 2), ("70X-x0070", 1), ("AAVNAR-x1513", 1), ("AAVNAR-x1705", 1)],
-    "Noise": [("STAG1A-x0579", 1), ("NSP16-x0357", 1), ("NSP16_x0415", 1), ("70X_x0137", 4), ("70X_x0109", 1)],
-    "Contaminant": [("NSP16_x0653", 1)],
+    "Noise": [("STAG1A-x0579", 1), ("NSP16-x0357", 1), ("NSP16-x0415", 1), ("70X-x0137", 4), ("70X-x0109", 1)],
+    "Contaminant": [("NSP16-x0653", 1)],
     "BVKP126": [("BKVP126-x0692", None)],
-    "NSP16_PARTIAL_LIG": [("NSP16-x0422", None), ("NSP16_x0489", None)],
+    "NSP16_PARTIAL_LIG": [("NSP16-x0422", None), ("NSP16-x0489", None)],
     "NSP16_CONFORTMATIONS": [("NSP16_x0415", 1)],
     "RSCC23": [
         ("LchARH3-x0073", None, 840,), ("AAVNAR-x1099", None, 783), ("XX02KALRNA-x1630", None, 491), ("Mpro-x0395", None, 494),
