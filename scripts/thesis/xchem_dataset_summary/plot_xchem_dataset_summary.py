5b9a5856c74ca0bc5fd6fa4cd1963eb38404a7d6
jfi22496 <conor.wild@sky.com>
2023-06-06 15:58:37 +0100

    Autobuild rank score default

--- scripts/thesis/xchem_dataset_summary/plot_xchem_dataset_summary.py
+++ scripts/thesis/xchem_dataset_summary/plot_xchem_dataset_summary.py
@@ -49,6 +49,7 @@ def plot_xchem_dataset_summaries():
     sqlite_filepath = "/dls/science/groups/i04-1/conor_dev/pandda_lib/diamond_2.db"
     sqlite_filepath = pathlib.Path(sqlite_filepath).resolve()
     output_dir = pathlib.Path("/dls/science/groups/i04-1/conor_dev/pandda_lib/thesis/xchem_dataset_summary")
+    output_table = output_dir / "dataset_statistics.csv"
     try_make(output_dir)
 
     # Get the database
@@ -58,64 +59,169 @@ def plot_xchem_dataset_summaries():
     session = sessionmaker(bind=engine)()
 
     # Get the information on each system
+
     initial_datasets: List[DatasetSQL] = session.query(DatasetSQL).options(subqueryload(DatasetSQL.bound_state_model)).order_by(
         DatasetSQL.id).all()
-    datasets_by_system = {}
-    for dataset in initial_datasets:
 
-        # Add the dataset to the datasets by system
-        system_name = get_system_from_dtag(dataset.dtag)
-        if not system_name:
-            continue
+    # Make a table and save it
+    if not output_table.exists():
+        records = []
+        for dataset in initial_datasets:
 
-        if system_name not in datasets_by_system:
-            datasets_by_system[system_name] = {}
-        datasets_by_system[system_name][dataset.dtag] = dataset
+            # Add the dataset to the datasets by system
+            system_name = get_system_from_dtag(dataset.dtag)
 
-    # For each system, get the relevant information and output a latex formated table
-    for system, system_datasets in datasets_by_system.items():
-        print(f"### {system}")
+            pdb_path = dataset.model_path
+            mtz_path = dataset.mtz_path
+            bound_state = dataset.bound_state_model
+            bound_state_path = dataset.pandda_model_path
 
+            # Censor if partial
+            accessible = False
+            if (not pdb_path) or (pdb_path == "None"):
+                if not pathlib.Path(pdb_path).exists():
+                    accessible = True
+            if (not mtz_path) or (mtz_path == "None"):
+                if not pathlib.Path(mtz_path).exists():
+                    accessible = True
+            if bound_state_path and (bound_state_path != "None"):
+                if not pathlib.Path(bound_state_path).exists():
+                    accessible = True
 
-        # Get number of datasets
-        num_datasets = len(system_datasets)
-        print(f"\tNumber of datasets: {num_datasets}")
+            if not accessible:
 
-        # Determine whethere the system is accessible
-        accessible_system_datasets = {}
-        for dtag, ds in system_datasets.items():
-            if pathlib.Path(ds.mtz_path).exists():
-                accessible_system_datasets[dtag] = ds
+                # Load the structure to get the properties
+                st = gemmi.read_structure(pdb_path)
 
-        print(f"\tNumber of accessible datasets: {len(accessible_system_datasets)}")
+                # Get the number of chains and residues
+                num_chains = 0
+                num_residues = 0
+                for model in st:
+                    for chain in model:
+                        num_chains += 1
+                        for res in chain:
+                            num_residues += 1
 
-        # Get minimum resolution
-        system_dataset_resolutions = [ ]
-        for ds in system_datasets.values():
-            if pathlib.Path(ds.mtz_path).exists():
-                system_dataset_resolutions.append(gemmi.read_mtz_file(ds.mtz_path).resolution_high())
+                # Get the spacegroup
+                sg = st.spacegroup_hm
+
+                # Get the resolution
+                res = st.resolution
+
+                # Get the unit cell volume
+                vol = st.cell.volume
+
+                # Get the fragment properties if there is a bound state model
+                if bound_state_path and (bound_state_path != "None"):
+
+                    bound_st = gemmi.read_structure(bound_state_path)
+
+                    # Get the hit status
+                    hit = True
+
+                    # Get the RSCC
+                    rscc = bound_state.rscc
+
+                    # Get the number of fragment atoms
+                    num_frag_atoms = 0
+                    for model in bound_st:
+                        for chain in model:
+                            for res in chain:
+                                # Don't double count
+                                if num_frag_atoms > 0:
+                                    continue
+                                if res.name in ["LIG", "XXX"]:
+                                    for atom in res:
+                                        if not atom.is_hydrogen():
+                                            num_frag_atoms += 1
+
+                else:
+                    hit = False
+                    rscc = None
+                    num_frag_atoms = None
             else:
-                print(ds.mtz_path)
-        if len(system_dataset_resolutions) == 0:
-            continue
-        min_res = min(system_dataset_resolutions)
+                res = None
+                sg = None
+                vol = None
+                num_residues = None
+                num_chains = None
+                rscc = None
+                hit = None
+                num_frag_atoms = None
 
-        # Get mean resolution
-        mean_res = sum(system_dataset_resolutions) / len(system_dataset_resolutions)
+            record = {
+                "System": system_name,
+                "Dtag": dataset.dtag,
+                "Resolution": res,
+                "Spacegroup": sg,
+                "Volume": vol,
+                "Number of Residues": num_residues,
+                "Number of Chains": num_chains,
+                "Accessible": accessible,
+                "Number of Fragment Heavy Atoms": num_frag_atoms,
+                "RSCC": rscc,
+                "Hit": hit
 
-        # Get max resolution
-        max_res = max(system_dataset_resolutions)
+            }
+            records.append(record)
 
-        # Get % with bound state model
-        project_hit_rate = len([x for x in system_datasets.values() if x.bound_state_model is not None]) / len(system_datasets)
+        # Output table of records
+        table = pd.DataFrame(records)
+        table.write_csv(output_table)
 
-        # Get the organism
 
-        # Get the protein id
-
-        #
-
-        print([system, num_datasets, min_res, mean_res, max_res, project_hit_rate])
+    #     if not system_name:
+    #         continue
+    #
+    #     if system_name not in datasets_by_system:
+    #         datasets_by_system[system_name] = {}
+    #     datasets_by_system[system_name][dataset.dtag] = dataset
+    #
+    # # For each system, get the relevant information and output a latex formated table
+    # records = []
+    # for system, system_datasets in datasets_by_system.items():
+    #     print(f"### {system}")
+    #
+    #
+    #     # Get number of datasets
+    #     num_datasets = len(system_datasets)
+    #     print(f"\tNumber of datasets: {num_datasets}")
+    #
+    #     # Determine whethere the system is accessible
+    #     accessible_system_datasets = {}
+    #     for dtag, ds in system_datasets.items():
+    #         if pathlib.Path(ds.mtz_path).exists():
+    #             accessible_system_datasets[dtag] = ds
+    #
+    #     print(f"\tNumber of accessible datasets: {len(accessible_system_datasets)}")
+    #
+    #     # Get minimum resolution
+    #     system_dataset_resolutions = [ ]
+    #     for ds in system_datasets.values():
+    #         if pathlib.Path(ds.mtz_path).exists():
+    #             system_dataset_resolutions.append(gemmi.read_mtz_file(ds.mtz_path).resolution_high())
+    #         else:
+    #             print(ds.mtz_path)
+    #     if len(system_dataset_resolutions) == 0:
+    #         continue
+    #     min_res = min(system_dataset_resolutions)
+    #
+    #     # Get mean resolution
+    #     mean_res = sum(system_dataset_resolutions) / len(system_dataset_resolutions)
+    #
+    #     # Get max resolution
+    #     max_res = max(system_dataset_resolutions)
+    #
+    #     # Get % with bound state model
+    #     project_hit_rate = len([x for x in system_datasets.values() if x.bound_state_model is not None]) / len(system_datasets)
+    #
+    #     # Get the organism
+    #
+    #     # Get the protein id
+    #
+    #     #
+    #
+    #     print([system, num_datasets, min_res, mean_res, max_res, project_hit_rate])
 
 
 if __name__ == "__main__":
