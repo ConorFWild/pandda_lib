ce803cf5ddc6731529b0114d769dec7e6ef25e2f
jfi22496 <conor.wild@sky.com>
2023-06-07 19:10:30 +0100

    Autobuild rank score default

--- scripts/thesis/xchem_dataset_summary/plot_map_uncertainties_vs_res.py
+++ scripts/thesis/xchem_dataset_summary/plot_map_uncertainties_vs_res.py
@@ -129,7 +129,7 @@ def plot_xchem_dataset_summaries():
     y_pred = regr.predict(X_test)
 
     # The coefficients
-    print("Coefficients: \n", regr.coef_)
+    print(f"Coefficients: {regr.intercept_float} {regr.coef_}\n" )
     # The coefficient of determination: 1 is perfect prediction
     print(f"R Squared: {r2_score(y_test, y_pred)}")
 
