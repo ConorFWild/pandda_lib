fc2f2898feaaafa60e3d938d6b1614ad0292a2cf
jfi22496 <conor.wild@sky.com>
2023-06-13 18:28:32 +0100

    p

--- scripts/thesis/system_tables/make_system_tables.py
+++ scripts/thesis/system_tables/make_system_tables.py
@@ -28,8 +28,10 @@ sns.set(font_scale=3)
 sns.set_palette("hls")
 sns.set_palette("crest")
 
+
 class SystemInfo:
     def __init__(self,
+                 published,
                  protein_name,
                  organism,
                  classification,
@@ -37,8 +39,9 @@ class SystemInfo:
                  pdb_deposition_group,
                  gene,
                  pandda_data_deposition,
-                literature
+                 literature
                  ):
+        self.published=published
         self.protein_name = protein_name
         self.organism = organism
         self.classification = classification
@@ -48,8 +51,10 @@ class SystemInfo:
         self.pandda_data_deposition = pandda_data_deposition
         self.literature = literature
 
+
 system_info = {
     "PDK2": SystemInfo(
+        published=False,
         protein_name="Pyruvate dehydrogenase kinase 2",
         organism="Homo sapiens",
         classification="Transferase",
@@ -60,6 +65,7 @@ system_info = {
         literature="10.1038/s42004-020-00367-0",
     ),
     "BKVP126": SystemInfo(
+        published=True,
         protein_name="Capsid protein VP1",
         organism="Merkel cell polyomavirus",
         classification="Viral protein",
@@ -70,16 +76,20 @@ system_info = {
         literature="10.1039/D2CB00052K",
     ),
     "NSP16": SystemInfo(
+        published=True,
+
         protein_name="Non-structural protein 16",
         organism="Severe acute respiratory syndrome coronavirus 2",
         classification="Viral protein",
         domain="2′-O-methyltransferase",
         pdb_deposition_group=["7B69", "7B6A", "7B6C"],
-        gene=None,#"orf1ab",
+        gene=None,  # "orf1ab",
         pandda_data_deposition="Fragalysis",
         literature=None,
     ),
     "STAG1A": SystemInfo(
+        published=True,
+
         protein_name="Cohesin subunit SA-1",
         organism="Homo sapiens",
         classification="Transcription",
@@ -90,6 +100,7 @@ system_info = {
         literature=None,
     ),
     "AAVNAR": SystemInfo(
+        published=False,
         protein_name="Cohesin subunit SA-1",
         organism="Homo sapiens",
         classification="Transcription",
@@ -101,6 +112,7 @@ system_info = {
     ),
 
     "70X": SystemInfo(
+        published=False,
         protein_name="Cohesin subunit SA-1",
         organism="Homo sapiens",
         classification="Transcription",
@@ -111,6 +123,7 @@ system_info = {
         literature="10.1107/S2053230X21007378",  # Actually right
     ),
     "XX02KALRNA": SystemInfo(
+        published=True,
         protein_name="Kalirin RhoGEF kinase in complex with Rac family small GTPase 1",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -120,8 +133,20 @@ system_info = {
         pandda_data_deposition="10.5281/zenodo.3271348",
         literature="10.1002/anie.201900585",
     ),
-    "HSP90": {},
+    "HSP90": SystemInfo(
+        published=False,
+
+        protein_name=None,
+        organism=None,
+        classification=None,
+        domain=None,
+        pdb_deposition_group=None,
+        gene=None,
+        pandda_data_deposition=None,
+        literature=None,
+    ),
     "BAZ2BA": SystemInfo(
+        published=True,
         protein_name="Bromodomain adjacent to zinc finger domain protein",
         organism="Homo sapiens",
         classification="DNA binding protein",
@@ -132,6 +157,8 @@ system_info = {
         literature=None,
     ),
     "JMJD2DA": SystemInfo(
+        published=True,
+
         protein_name="Lysine-specific demethylase 4D",
         organism="Homo sapiens",
         classification="Oxidoreductase",
@@ -142,6 +169,8 @@ system_info = {
         literature=None,
     ),
     "Mpro": SystemInfo(
+        published=True,
+
         protein_name="Mpro",
         organism="Severe acute respiratory syndrome coronavirus 2",
         classification="Hydrolase/hydrolase inhibitor",
@@ -152,6 +181,8 @@ system_info = {
         literature="10.1038/s41467-020-18709-w",
     ),
     "Mac1": SystemInfo(
+        published=True,
+
         protein_name="Non-structural protein 3",
         organism="Severe acute respiratory syndrome coronavirus 2",
         classification="Hydrolase",
@@ -162,6 +193,7 @@ system_info = {
         literature="10.1101/2020.11.24.393405",
     ),
     "NME4": SystemInfo(
+        published=False,
         protein_name="Nucleoside diphosphate kinase, mitochondrial",
         organism="Homo sapiens",
         classification="Transferase",
@@ -172,6 +204,7 @@ system_info = {
         literature=None,
     ),
     "GN6S": SystemInfo(
+        published=False,
         protein_name="Glucosamine (N-acetyl)-6-sulfatase",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -182,6 +215,8 @@ system_info = {
         literature=None,
     ),
     "SOCS2A": SystemInfo(
+        published=False,
+
         protein_name="Suppressor of cytokine signaling 2",
         organism="Homo sapiens",
         classification="Ligase",
@@ -192,6 +227,7 @@ system_info = {
         literature="10.26434/chemrxiv-2022-bvj80",
     ),
     "B2m": SystemInfo(
+        published=False,
         protein_name="Beta-2-microglobulin",
         organism="Homo sapiens",
         classification="Histocompatibility antigen",
@@ -202,6 +238,7 @@ system_info = {
         literature=None,
     ),
     "LcARH3": SystemInfo(
+        published=False,
         protein_name="Beta-2-microglobulin",
         organism="Homo sapiens",
         classification="Histocompatibility antigen",
@@ -212,6 +249,7 @@ system_info = {
         literature=None,
     ),
     "SHH": SystemInfo(
+        published=False,
         protein_name="Sonic hedgehog protein",
         organism="Homo sapiens",
         classification="Signalling protein",
@@ -222,6 +260,7 @@ system_info = {
         literature=None,
     ),
     "PHIPA": SystemInfo(
+        published=True,
         protein_name="Pleckstrin Homology Domain Interacting Protein",
         organism="Homo sapiens",
         classification="Signalling protein",
@@ -232,6 +271,7 @@ system_info = {
         literature="10.1039/c5sc03115j",
     ),
     "NUDT22A": SystemInfo(
+        published=True,
         protein_name="Uridine diphosphate glucose pyrophosphatase NUDT22",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -242,6 +282,7 @@ system_info = {
         literature=None,
     ),
     "DCP2B": SystemInfo(
+        published=True,
         protein_name="mRNA-decapping enzyme 2",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -252,6 +293,8 @@ system_info = {
         literature=None,
     ),
     "TbMDO": SystemInfo(
+        published=False,
+
         protein_name="mRNA-decapping enzyme 2",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -262,6 +305,8 @@ system_info = {
         literature=None,
     ),
     "SHMT2A": SystemInfo(
+        published=False,
+
         protein_name="Serine hydroxymethyltransferase, mitochondrial",
         organism="Homo sapiens",
         classification="Transferase",
@@ -272,6 +317,7 @@ system_info = {
         literature=None,
     ),
     "Lc_TbrB1": SystemInfo(
+        published=False,
         protein_name="Serine hydroxymethyltransferase, mitochondrial",
         organism="Homo sapiens",
         classification="Transferase",
@@ -282,6 +328,7 @@ system_info = {
         literature=None,
     ),
     "TBXTA": SystemInfo(
+        published=True,
         protein_name="T-box transcription factor T",
         organism="Homo sapiens",
         classification="Transcription",
@@ -291,10 +338,45 @@ system_info = {
         pandda_data_deposition="Fragalysis",
         literature=None,
     ),
-    "KPNF": {},
-    "CYCK": {},
-    "P1Pro": {},
+    "KPNF": SystemInfo(
+        published=False,
+
+        protein_name=None,
+        organism=None,
+        classification=None,
+        domain=None,
+        pdb_deposition_group=None,
+        gene=None,
+        pandda_data_deposition=None,
+        literature=None,
+    ),
+    "CYCK": SystemInfo(
+        published=False,
+
+        protein_name=None,
+        organism=None,
+        classification=None,
+        domain=None,
+        pdb_deposition_group=None,
+        gene=None,
+        pandda_data_deposition=None,
+        literature=None,
+    ),
+    "P1Pro": SystemInfo(
+        published=False,
+
+        protein_name=None,
+        organism=None,
+        classification=None,
+        domain=None,
+        pdb_deposition_group=None,
+        gene=None,
+        pandda_data_deposition=None,
+        literature=None,
+    ),
     "PKM1": SystemInfo(
+        published=False,
+
         protein_name="Pyruvate kinase M1",
         organism="Homo sapiens",
         classification="Kinase",
@@ -305,6 +387,7 @@ system_info = {
         literature=None,
     ),
     "TRF1": SystemInfo(
+        published=False,
         protein_name="Telomeric repeat-binding factor 1",
         organism="Homo sapiens",
         classification="Telomere binding",
@@ -315,6 +398,7 @@ system_info = {
         literature=None,
     ),
     "G13D": SystemInfo(
+        published=False,
         protein_name="Telomeric repeat-binding factor 1",
         organism="Homo sapiens",
         classification="Telomere binding",
@@ -325,6 +409,7 @@ system_info = {
         literature=None,
     ),
     "JMJD1BA": SystemInfo(
+        published=True,
         protein_name="Lysine-specific demethylase 3B",
         organism="Homo sapiens",
         classification="Oxidoreductase",
@@ -335,6 +420,7 @@ system_info = {
         literature=None,
     ),
     "ATAD2A": SystemInfo(
+        published=True,
         protein_name="ATPase family AAA domain-containing protein 2",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -345,6 +431,7 @@ system_info = {
         literature="10.1039/C8OB00099A",
     ),
     "mkeap1": SystemInfo(
+        published=True,
         protein_name="Kelch-like ECH-associated protein 1",
         organism="Homo sapiens",
         classification="Transcription",
@@ -355,6 +442,7 @@ system_info = {
         literature=None,
     ),
     "MACRO1DA": SystemInfo(
+        published=False,
         protein_name="ADP-ribose glycohydrolase MACROD1",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -365,6 +453,8 @@ system_info = {
         literature=None,
     ),
     "DCLRE1AA": SystemInfo(
+        published=True,
+
         protein_name="DNA cross-link repair 1A protein",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -375,6 +465,7 @@ system_info = {
         literature=None,
     ),
     "TNCA": SystemInfo(
+        published=True,
         protein_name="Tenascin-C",
         organism="Homo sapiens",
         classification="Cell adhesion",
@@ -385,6 +476,7 @@ system_info = {
         literature=None,
     ),
     "BRD1": SystemInfo(
+        published=True,
         protein_name="Bromodomain-containing protein 1",
         organism="Homo sapiens",
         classification="Gene regulation",
@@ -395,6 +487,7 @@ system_info = {
         literature=None,
     ),
     "PTP1B": SystemInfo(
+        published=True,
         protein_name="Tyrosine-protein phosphatase non-receptor type 1",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -405,6 +498,7 @@ system_info = {
         literature="10.7554/eLife.36307",
     ),
     "FALZA": SystemInfo(
+        published=True,
         protein_name="Tyrosine-protein phosphatase non-receptor type 1",
         organism="Homo sapiens",
         classification="Hydrolase",
@@ -417,12 +511,85 @@ system_info = {
 
 }
 
+sgc_summaries = [
+    "mArh",
+    "INPP5DA",
+    "NUDT21A",
+    "FAM83BA",
+    "NUDT22A",
+    "NUDT4A",
+    "PARP14A",
+    "HAO1A",
+    "DCLRE1AA",
+    "NUDT5A",
+    "NUDT7A",
+    "FALZA",
+    "SP100A",
+    "BAZ2BA",
+    "BRD1A",
+    "JMJD2DA",
+    "ATAD2",
+    "KDM5B",
+    "JMJD1B",
+]
+
+fragalysis = [
+    "EPB41L3A",
+    "HAO1A",
+    "NUDT4",
+    "STAG1A",
+    "MURD",
+    "NSP16",
+    "PGN_RS02895PGA",
+    "NSP15_B",
+    "nsp13",
+    "FAM83BA",
+    "ALAS2A",
+    "MUREECA",
+    "ATAD2A",
+    "SMTGR",
+    "VIM2",
+    "OXA10OTA",
+    "Nprot",
+    "ACVR1A",
+    "PARP14A",
+    "NUDT7A_CRUDE",
+    "XX02KALRNA",
+    "CAMK1DA",
+    "ATAD",
+    "DCP2B",
+    "BRD1A",
+    "SOS3ATA",
+    "mArh",
+    "TBXTA",
+    "NUDT7A",
+    "D68EV3CPROA",
+    "macro-combi",
+    "FALZA",
+    "NSP14",
+    "CD44MMA",
+    "TNCA",
+    "smTGRNEW",
+    "PTP1B",
+    "NUDT4A",
+    "Mpro",
+    "MID2A",
+    "MUREECOLI",
+    "NUDT21A",
+    "Mac1",
+    "DCLRE1AA",
+    "INPP5DA",
+    "NUDT5A",
+    "PHIPA",
+]
+
 
 def try_make(path):
     try:
         os.mkdir(path)
     except Exception as e:
         return
+
 
 def get_system_from_dtag(dtag):
     hyphens = [pos for pos, char in enumerate(dtag) if char == "-"]
@@ -432,6 +599,8 @@ def get_system_from_dtag(dtag):
         last_hypen_pos = hyphens[-1]
         system_name = dtag[:last_hypen_pos]
         return system_name
+
+
 def make_system_tables():
     for key in sorted(system_info):
         print(key)
@@ -439,7 +608,8 @@ def make_system_tables():
 
     sqlite_filepath = "/dls/science/groups/i04-1/conor_dev/pandda_lib/diamond_2.db"
     sqlite_filepath = pathlib.Path(sqlite_filepath).resolve()
-    output_dir = pathlib.Path("/dls/science/groups/i04-1/conor_dev/pandda_lib/thesis")
+    output_dir = pathlib.Path("/dls/science/groups/i04-1/conor_dev/pandda_lib/thesis/xchem_dataset_summary")
+    output_table = output_dir / "dataset_statistics.csv"
     try_make(output_dir)
 
     # Get the database
@@ -449,30 +619,79 @@ def make_system_tables():
     session = sessionmaker(bind=engine)()
 
     # Get the information on each system
-    initial_datasets = session.query(DatasetSQL).options(subqueryload(DatasetSQL.bound_state_model)).order_by(
-        DatasetSQL.id).all()
-    datasets_by_system = {}
-    for dataset in initial_datasets:
+    # initial_datasets = session.query(DatasetSQL).options(subqueryload(DatasetSQL.bound_state_model)).order_by(
+    #     DatasetSQL.id).all()
+    # datasets_by_system = {}
+    # for dataset in initial_datasets:
+    #
+    #     # Add the dataset to the datasets by system
+    #     system_name = get_system_from_dtag(dataset.dtag)
+    #     if not system_name:
+    #         continue
+    #
+    #     if not system_name in datasets_by_system:
+    #         datasets_by_system[system_name] = {}
+    #     datasets_by_system[system_name][dataset.dtag] = dataset
 
-        # Add the dataset to the datasets by system
-        system_name = get_system_from_dtag(dataset.dtag)
-        if not system_name:
-            continue
 
-        if not system_name in datasets_by_system:
-            datasets_by_system[system_name] = {}
-        datasets_by_system[system_name][dataset.dtag] = dataset
+    table = pd.read_csv(output_table)
+
+    system_hit_rate_records = []
+    for system in table[table["Accessible"] == True]["System"].unique():
+        system_table = table[table["System"] == system]
+        num_hits = len(system_table[system_table["RSCC"] > 0.0]["Dtag"].unique())
+        num_datasets = len(system_table["Dtag"].unique())
+        num_accessible_datasets = len(system_table[system_table["Accessible"] == True]["Dtag"].unique())
+        if num_datasets != 0:
+            system_hit_rate_records.append(
+                {
+                    "System": system,
+                    "Hit Rate": num_hits / num_datasets,
+                    "Number of Hits": num_hits,
+                    "Number of Datasets": num_datasets,
+                    "Number of Accessible Datasets": num_accessible_datasets
+                })
+
+    system_hit_rate_table = pd.DataFrame(system_hit_rate_records)
 
     # For each system, get the relevant information and output a latex formated table
-    for system, system_datasets in datasets_by_system.items():
+    for system in table[table["Accessible"] == True]["System"].unique():
+
+        system_table = table[table["System"] == system]
+
         # Get number of datasets
+        num_datasets = len(system_table["Dtag"].unique())
+        num_accessible_datasets = len(system_table[system_table["Accessible"] == True]["Dtag"].unique())
+
+        # Get the number of hits
+        num_hits = len(system_table[system_table["RSCC"] > 0.0]["Dtag"].unique())
+
+        # # Get the structures
+        # structures = {}
+        # for system_dataset in system_datasets:
+        #     structures[system_dataset.dtag] = gemmi.read_structure(system_dataset.model_path)
+        #
+        # # Get the resolutions
+        # resolutions = [st.resolution for st in structures.values()]
 
         # Get minimum resolution
+        min_res = system_table["Resolution"].min()
 
         # Get mean resolution
+        median_res = system_table["Resolution"].median()
 
         # Get max resolution
+        max_res = system_table["Resolution"].max()
 
+        # Spacegroups
+        sgs =         system_table["Spacegroup"]
+        unique_sgs, counts = np.unique(sgs, return_counts=True)
+
+        # Num chains
+        num_chains = system_table["Number of Chains"].unique()
+        num_residues = system_table["Number of Residues"].unique()
+
+        volume = system_table["Volume"].unique()
         # Get % with bound state model
 
         # Get the organism
@@ -480,6 +699,18 @@ def make_system_tables():
         # Get the protein id
 
         #
+        print(f"##### {system} #####")
+        print(num_datasets)
+        print(num_accessible_datasets)
+        print(num_hits)
+        print(min_res)
+        print(median_res)
+        print(max_res)
+        print(unique_sgs)
+        print(counts)
+        print(num_chains)
+        print(num_residues)
+        print(volume)
 
         ...
 
